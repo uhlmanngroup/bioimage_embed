68665e1b55b0a0653737e571ddaefd9eef5e1d15
ctr26 <ctr26@ebi.ac.uk>
2021-07-19 15:29:20 +0100

    Adding AE (not a VAE)

--- idr_get_data.py
+++ idr_get_data.py
@@ -14,6 +14,12 @@ import matplotlib.pyplot as plt
 from torchvision.datasets.folder import default_loader
 from PIL import Image
 
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
 
 # https://pytorch.org/docs/stable/data.html#torch.utils.data.Dataset
 # https://pytorch.org/vision/stable/auto_examples/plot_scripted_tensor_transforms.html#sphx-glr-auto-examples-plot-scripted-tensor-transforms-py
@@ -169,3 +175,69 @@ for i, (rgb, gt) in enumerate(dataloader):
 plt.imshow(get_idr_image(4684936))
 
 # %%
+import torch
+import torchvision
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import save_image
+from torchvision.datasets import MNIST
+import os
+
+# https://github.com/L1aoXingyu/pytorch-beginner/blob/master/08-AutoEncoder/conv_autoencoder.py
+
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+            nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+num_epochs = 100
+batch_size = 128
+learning_rate = 1e-3
+
+
+model = autoencoder().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
+                             weight_decay=1e-5)
+total_loss = 0
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        img = Variable(img).cuda()
+        # ===================forward=====================
+        output = model(img)
+        loss = criterion(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    total_loss += loss.data
+    print(f'epoch [{epoch+1}/{num_epochs}], loss:{total_loss}')
+    if epoch % 10 == 0:
+        pic = to_img(output.cpu().data)
+        save_image(pic, f'./dc_img/image_{str(epoch)}.png')
+
+torch.save(model.state_dict(), './conv_autoencoder.pth')
