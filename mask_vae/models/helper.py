0eb204a97a4cfc19cce6e4a1bd76693324b832ef
Craig <ctr26@ebi.ac.uk>
2022-03-12 19:22:50 +0000

    Adding helper function for future base class

---
+++ mask_vae/models/helper.py
@@ -0,0 +1,44 @@
+
+import sys
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from pyro.optim import Adam
+from pyro.infer import SVI, Trace_ELBO
+import pyro.distributions as dist
+import pyro
+import pytorch_lightning as pl
+from torch.utils.data import random_split, DataLoader
+import glob
+# Note - you must have torchvision installed for this example
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import os
+from skimage.measure import regionprops
+from torchvision.transforms.functional import crop
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch import nn
+from pytorch_lightning import loggers as pl_loggers
+import torchvision
+from sklearn.manifold import MDS  
+from sklearn.metrics.pairwise import euclidean_distances
+from scipy.ndimage import convolve,sobel 
+from skimage.measure import find_contours
+from scipy.interpolate import interp1d
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from mask_vae.transforms import DistogramToMaskPipeline
+
+class AutoEncoderBase(nn.Module):
+    def __init__():
+        super().__init__()
+    def decoder(self,z):
+        raise NotImplementedError
+    def encoder(self,img):
+        raise NotImplementedError
