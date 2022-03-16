658bfe39953860c5c972102095ef3e348ee314a5
Craig <ctr26@ebi.ac.uk>
2022-03-16 08:47:56 +0000

    Adding umap to generator

--- generator.py
+++ generator.py
@@ -7,6 +7,9 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
+import umap 
+import umap.plot
+from sklearn.decomposition import PCA
 
 #  %%
 from ast import excepthandler
@@ -75,9 +78,9 @@ learning_rate = 1e-3
 train_dataset_glob = os.path.join(os.path.expanduser("~"),
                                   "data-science-bowl-2018/stage1_train/*/masks/*.png")
 
-def my_collate(batch):
-    batch = list(filter(lambda x: x is not None, batch))
-    return torch.utils.data.dataloader.default_collate(batch)
+# def my_collate(batch):
+#     batch = list(filter(lambda x: x is not None, batch))
+#     return torch.utils.data.dataloader.default_collate(batch)
 
 # test_dataloader_glob=os.path.join(os.path.expanduser("~"),
 # "data-science-bowl-2018/stage1_test/*/masks/*.png")
@@ -117,6 +120,7 @@ model = LitAutoEncoder(model).load_from_checkpoint(
 # model = lit_model.load_from_checkpoint(checkpoint_path="checkpoints/last.ckpt")
 # test_img = torch.tensor(np.zeros((1, 96, 96)), dtype=torch.float32)
 # train_dataset = DSB2018(train_dataset_glob, transform=transformer_dist)
+
 test_mask = train_dataset_crop[1].unsqueeze(0)
 test_img = train_dataset[1].unsqueeze(0)
 plt.imshow(test_img[0][0])
@@ -125,17 +129,90 @@ loss,y_prime,_, = model.forward(test_img)
 y_prime = y_prime.detach().numpy()
 plt.imshow(y_prime[0][0])
 plt.show()
+#  %%
 z = model.encoder(test_img)
 y_prime = model.decoder(z).detach().numpy()
 plt.imshow(y_prime[0][0])
 plt.show()
 # print(z)
+# z = model.model.model._encoder(test_img)
+# z = model.model.model._pre_vq_conv(z)
+#  %%
+from tqdm import tqdm
+z_list = []
+for data in tqdm(train_dataset):
+    if data is not None:
+        z_list.append(model.encoder(data))
+    if (len(z_list) >= 1000):
+        break
+latent = torch.stack(z_list).detach().numpy()
+#  %%
+latent_umap = latent.reshape(latent.shape[0],-1)
+unfit_umap = umap.UMAP(n_neighbors=3,
+                 min_dist=0.1,
+                 metric='cosine')
+unfit_umap = umap.UMAP()
+
+fit_umap = unfit_umap.fit(latent_umap)
+proj = fit_umap.transform(latent_umap)
+
+umap.plot.points(fit_umap)
+plt.show()
+plt.scatter(proj[:,0],proj[:,1])
+plt.show()
+#  %%
+pc_1 = fit_umap.inverse_transform(np.array([[1,0]]))
+pc_2 = fit_umap.inverse_transform(np.array([[0,1]]))
+
+plt.plot(pc_1.T)
+plt.show()
+plt.plot(pc_2.T)
+plt.show()
+# %%
+coord_1 = np.array([[-1,7]])
+coord_2 = np.array([[0,-4]])
+
+cluster_1 = fit_umap.inverse_transform(coord_1)
+cluster_2 = fit_umap.inverse_transform(coord_2)
+
+cluster_1_z = cluster_1.reshape((z.shape))
+cluster_2_z = cluster_2.reshape((z.shape))
+
+z_cluster_1 = model.decoder(torch.tensor(cluster_1_z)).detach().numpy()
+z_cluster_2 = model.decoder(torch.tensor(cluster_2_z)).detach().numpy()
+
+mask_1 = AsymmetricDistogramToMaskPipeline(window_size)(z_cluster_1)
+mask_2 = AsymmetricDistogramToMaskPipeline(window_size)(z_cluster_2)
+
+plt.title("coord_1")
+plt.imshow(mask_1[0][0])
+plt.show()
+plt.title("coord_2")
+plt.imshow(mask_2[0][0])
+plt.show()
+
+plt.scatter(proj[:,0],proj[:,1])
+plt.scatter(coord_1[:,0],coord_1[:,1],label="coord_1")
+plt.scatter(coord_2[:,0],coord_2[:,1],label="coord_2")
+plt.legend()
+plt.show()
+#  %%
+
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
+
+hull = ConvexHull(proj)
+plt.plot(proj[hull.vertices,0], proj[hull.vertices,1], 'r--', lw=2)
+#  %%
+
+from scipy.stats import gaussian_kde
+
+kde = gaussian_kde(proj)
+
+#  %%
 
 # %%
 embed = model.get_embedding()
 
-import umap 
-from sklearn.decomposition import PCA
 proj = PCA().fit_transform(embed)
 # proj = umap.UMAP(n_neighbors=3,
 #                  min_dist=0.1,
@@ -145,7 +222,7 @@ fit_umap = umap.UMAP(n_neighbors=3,
                  metric='cosine').fit(embed)
 proj = fit_umap.transform(embed)
 
-pc_1 = fit_umap.inverse_transform(np.array([[1,0]]))
+pc_1 = fit_umap.inverse_transform(np.array([[0,10]]))
 pc_2 = fit_umap.inverse_transform(np.array([[0,1]]))
 
 plt.scatter(proj[:,0],proj[:,1])
@@ -161,7 +238,7 @@ fig, ax = plt.subplots(10,2,figsize=(4, 20))
 for i in range(10):
     z_random = torch.normal(torch.zeros_like(z), torch.ones_like(z))
     # z_random = torch.ones_like(z)
-    z_random = z + torch.normal(torch.zeros_like(z), torch.ones_like(z))/5
+    z_random = z + torch.normal(torch.zeros_like(z), torch.ones_like(z))/2
     generated_image_dist = model.decoder(z_random).detach().numpy()
     # dist_stack = np.stack(
     #     [generated_image_dist[0], np.transpose(generated_image_dist[0],)], axis=0)
