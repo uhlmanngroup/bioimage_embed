450c993faddb503dea18ff8a5094c9b6d505d2a6
Craig <ctr26@ebi.ac.uk>
2022-03-18 11:58:06 +0000

    adding data aug to training

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import torch
 from torch.utils.data import DataLoader
 import os
 from pytorch_lightning import loggers as pl_loggers
+from torchvision import transforms
 
 
 from mask_vae.datasets import DSB2018
@@ -17,6 +18,8 @@ from mask_vae.models import Mask_VAE, VQ_VAE, VAE
 from mask_vae.lightning import LitAutoEncoder
 
 interp_size = 128*4
+
+max_epochs=150
 
 window_size = 128-32
 batch_size = 32
@@ -37,11 +40,18 @@ learning_rate = 1e-3
 
 train_dataset_glob = os.path.join(os.path.expanduser("~"),
                                   "data-science-bowl-2018/stage1_train/*/masks/*.png")
+
+train_dataset_glob = os.path.join("data/BBBC010_v1_foreground_eachworm/*.png")
+
+
 # train_dataset_glob = os.path.join("data/dsb2018/train/masks/*.tif")
 # test_dataloader_glob=os.path.join(os.path.expanduser("~"),
 # "data-science-bowl-2018/stage1_test/*/masks/*.png")
 
 model_dir = "test"
+model_dir = "BBBC010_v1_foreground_eachworm"
+
+
 transformer_crop = CropCentroidPipeline(window_size)
 transformer_dist = MaskToDistogramPipeline(window_size, interp_size)
 transformer_coords = DistogramToCoords(window_size)
@@ -49,7 +59,21 @@ transformer_coords = DistogramToCoords(window_size)
 # train_dataset_raw = DSB2018(train_dataset_glob)
 # train_dataset_crop = DSB2018(
 #     train_dataset_glob, transform=CropCentroidPipeline(window_size))
+
+
+transform = transforms.Compose(
+    [
+        transformer_crop,
+        transforms.RandomRotation(360),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
+        transformer_dist,
+
+    ]
+)
+
 train_dataset = DSB2018(train_dataset_glob, transform=transformer_dist)
+
 
 # img_squeeze = train_dataset_crop[0].unsqueeze(0)
 # img_crop = train_dataset_crop[0]
@@ -84,7 +108,7 @@ trainer = pl.Trainer(
     accumulate_grad_batches=1,
     callbacks=[checkpoint_callback],
     min_epochs=50,
-    max_epochs=75,
+    max_epochs=max_epochs,
 )  # .from_argparse_args(args)
 
 
