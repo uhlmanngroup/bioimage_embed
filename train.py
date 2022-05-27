26132f8d489c960374db853ef4a7a6e73ad493bd
Craig <ctr26@ebi.ac.uk>
2022-05-27 16:20:57 +0100

    updating training

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+# %%
 import os
 from pathlib import Path
 
@@ -16,12 +17,14 @@ from mask_vae.datasets import DSB2018
 from mask_vae.transforms import CropCentroidPipeline, DistogramToCoords, DistogramToCoords, MaskToDistogramPipeline
 from mask_vae.models import Mask_VAE, VQ_VAE, VAE
 from mask_vae.lightning import LitAutoEncoderTorch, LitAutoEncoderPyro
-interp_size = 128*2
+import matplotlib.pyplot as plt
 
-max_epochs=150
+interp_size = 128*4
 
-window_size = 128-32
-batch_size = 32
+max_epochs = 5000
+
+window_size = 128*4
+batch_size = 2
 num_training_updates = 15000
 
 num_hiddens = 64
@@ -35,27 +38,28 @@ commitment_cost = 0.25
 
 decay = 0.99
 
-learning_rate = 1e-3
+learning_rate = 1e-4
 
-train_dataset_glob = os.path.join(os.path.expanduser("~"),
-                                  "data-science-bowl-2018/stage1_train/*/masks/*.png")
+# train_dataset_glob = "data-science-bowl-2018/stage1_train/*/masks/*.png"
+train_dataset_glob = "data/stage1_train/*/masks/*.png"
 
-train_dataset_glob = os.path.join("data/BBBC010_v1_foreground_eachworm/*.png")
+# train_dataset_glob = os.path.join("data/BBBC010_v1_foreground_eachworm/*.png")
 
 
 # train_dataset_glob = os.path.join("data/dsb2018/train/masks/*.tif")
 # test_dataloader_glob=os.path.join(os.path.expanduser("~"),
 # "data-science-bowl-2018/stage1_test/*/masks/*.png")
 
-model_dir = "test"
-model_dir = "BBBC010_v1_foreground_eachworm"
-
+# model_dir = "test"
+# model_dir = "BBBC010_v1_foreground_eachworm"
+model_dir = "dsb2018"
+# %%
 
 transformer_crop = CropCentroidPipeline(window_size)
 transformer_dist = MaskToDistogramPipeline(window_size, interp_size)
 transformer_coords = DistogramToCoords(window_size)
 
-# train_dataset_raw = DSB2018(train_dataset_glob)
+train_dataset = DSB2018(train_dataset_glob)
 # train_dataset_crop = DSB2018(
 #     train_dataset_glob, transform=CropCentroidPipeline(window_size))
 
@@ -63,19 +67,29 @@ transformer_coords = DistogramToCoords(window_size)
 transform = transforms.Compose(
     [
         transformer_crop,
-        transforms.RandomRotation(360),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomHorizontalFlip(),
         transformer_dist,
 
     ]
 )
 
 train_dataset = DSB2018(train_dataset_glob, transform=transformer_dist)
+plt.imshow(train_dataset[0][0],cmap='gray')
+plt.show()
+
+train_dataset = DSB2018(train_dataset_glob, transform=transforms.ToTensor())
+plt.imshow(train_dataset[0][0],cmap='gray')
+plt.show()
+train_dataset = DSB2018(train_dataset_glob, transform=transformer_crop)
+plt.imshow(train_dataset[0][0],cmap='gray')
+plt.show()
 
 
-# img_squeeze = train_dataset_crop[0].unsqueeze(0)
-# img_crop = train_dataset_crop[0]
+train_dataset = DSB2018(train_dataset_glob, transform=transformer_crop)
+plt.imshow(train_dataset[0][0],cmap='gray')
+plt.show()
+
+# img_squeeze = train_dataset[0].unsqueeze(0)
+# %%
 
 
 def my_collate(batch):
@@ -84,10 +98,12 @@ def my_collate(batch):
 
 
 dataloader = DataLoader(train_dataset, batch_size=batch_size,
-                        shuffle=True, num_workers=8, pin_memory=True, collate_fn=my_collate)
+                        shuffle=True, num_workers=2**8, pin_memory=True, collate_fn=my_collate)
 
 model = Mask_VAE(VQ_VAE(channels=1))
-model = Mask_VAE(VAE(1, 64, image_dims=(interp_size, interp_size)))
+model = Mask_VAE(VAE(1, 64,
+                     #  hidden_dims=[32, 64],
+                     image_dims=(interp_size, interp_size)))
 
 # model = Mask_VAE(VAE())
 
@@ -112,9 +128,11 @@ trainer = pl.Trainer(
     max_epochs=max_epochs,
 )  # .from_argparse_args(args)
 
-
+# %%
 try:
     trainer.fit(lit_model, dataloader,
                 ckpt_path=f'{model_dir}/last.ckpt')
 except:
     trainer.fit(lit_model, dataloader)
+
+# %%
