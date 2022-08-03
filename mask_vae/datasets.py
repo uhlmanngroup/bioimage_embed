e450f82d0d77ec3415404470813fe4fe6878ce91
Craig <ctr26@ebi.ac.uk>
2022-08-03 10:15:41 +0100

    Adding more wrapping classes to general Broad

--- mask_vae/datasets.py
+++ mask_vae/datasets.py
@@ -2,6 +2,7 @@
 import sys
 from torch.utils.data import random_split, DataLoader
 import glob
+
 # Note - you must have torchvision installed for this example
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
@@ -11,7 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch import nn
-from scipy.ndimage import convolve,sobel 
+from scipy.ndimage import convolve, sobel
 
 from scipy.interpolate import interp1d
 import torch
@@ -21,29 +22,36 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from urllib.error import URLError
 
-from torchvision.datasets.utils import check_integrity, download_and_extract_archive, extract_archive, verify_str_arg
-
+from torchvision.datasets.utils import (
+    check_integrity,
+    download_and_extract_archive,
+    extract_archive,
+    verify_str_arg,
+)
 
 
 class DatasetGlob(Dataset):
-    def __init__(self, path_glob, transform=None,**kwargs):
+    def __init__(self, path_glob, transform=None, **kwargs):
         self.image_paths = glob.glob(path_glob, recursive=True)
         self.transform = transform
-        
+
     # def make_subset(self, index):
     #     self.getitem(index)
 
     def is_image_cropped(image):
-        if (np.sum(
-            np.sum(image[:,0]),
-            np.sum(image[:,-1]),
-            np.sum(image[0,:]),
-            np.sum(image[-1,:])
-        ) == 0):
+        if (
+            np.sum(
+                np.sum(image[:, 0]),
+                np.sum(image[:, -1]),
+                np.sum(image[0, :]),
+                np.sum(image[-1, :]),
+            )
+            == 0
+        ):
             return False
         else:
             return True
-    
+
     def getitem(self, index):
         try:
             x = Image.open(self.image_paths[index])
@@ -52,18 +60,18 @@ class DatasetGlob(Dataset):
             return x
         except:
             return None
-        
+
     def __getitem__(self, index):
         # x = self.getitem(index)
         # if self.is_image_cropped(x):
         # return index
         # if isinstance(index, slice):
-            # return [self.getitem(i) for i in range(*index.indices(10))]
+        # return [self.getitem(i) for i in range(*index.indices(10))]
         # TODO implement indexing/slicing
         # return self.getitem(index)
 
         # return self.getitem(index)
-        dummy_list = np.arange(0,self.__len__())
+        dummy_list = np.arange(0, self.__len__())
         loop = np.array(dummy_list[index])
         if isinstance(index, slice):
             return [self.getitem(i) for i in loop]
@@ -74,32 +82,38 @@ class DatasetGlob(Dataset):
 
     def __len__(self):
         return len(self.image_paths)
-    
-class BBBC010(DatasetGlob):
-    url = "https://data.broadinstitute.org/bbbc/BBBC010/BBBC010_v1_foreground_eachworm.zip"
-    md5 = "ae73910ed293397b4ea5082883d278b1"
-    dataset = "bbbc010"
-    # folder_name = "BBBC010_v1_foreground_eachworm"
-    images_file = "BBBC010_v1_foreground_eachworm.zip"
-    filename = images_file
-    
-    def __init__(self, data_folder="data", download=True, transform=None, **kwargs):
 
+
+class WebArchiveDataset(DatasetGlob):
+    def __init__(
+        self,
+        url,
+        md5,
+        filename,
+        dataset,
+        data_folder="data",
+        download=True,
+        transform=None,
+        **kwargs,
+    ):
+        self.url = url
+        self.md5 = md5
+        self.dataset = dataset
         self.raw_folder = f"{data_folder}/{self.dataset}"
+        self.filename = filename
+        self.images_file = self.filename
         path_glob = f"{self.raw_folder}/**/*.png"
-        
+
         # self.image_paths = glob.glob(path_glob, recursive=True)
         # self.transform = transform
         if download:
             self.download()
-        super(BBBC010, self).__init__(path_glob, transform, **kwargs)
+        super(WebArchiveDataset, self).__init__(path_glob, transform, **kwargs)
 
     def _check_exists(self) -> bool:
         return all(check_integrity(file) for file in (self.images_file))
 
     def download(self) -> None:
-        
-        """Download the BBBC010 data if it doesn't exist already."""
 
         if self._check_exists():
             return
@@ -110,6 +124,37 @@ class BBBC010(DatasetGlob):
 
         try:
             print(f"Downloading {self.url}")
-            download_and_extract_archive(self.url, download_root=self.raw_folder, filename=self.filename, md5=self.md5)
+            download_and_extract_archive(
+                self.url,
+                download_root=self.raw_folder,
+                filename=self.filename,
+                md5=self.md5,
+            )
         except URLError as error:
             print(f"Failed to download (trying next):\n{error}")
+
+
+class BroadDataset(WebArchiveDataset):
+    lookup_info = {
+        "BBBC010": {
+            "url": "https://data.broadinstitute.org/bbbc/BBBC010/BBBC010_v1_foreground_eachworm.zip",
+            "md5": "ae73910ed293397b4ea5082883d278b1",
+            "dataset": "bbbc010",
+            "filename": "BBBC010_v1_foreground_eachworm.zip",
+        }
+    }
+
+    def __init__(
+        self,
+        image_set="BBBC010",**kwargs,
+    ):
+        super(BroadDataset, self).__init__(
+            **self.lookup_info[image_set],**kwargs,
+        )
+
+
+class BBBC010(BroadDataset):
+    def __init__(self,*args,**kwargs):
+        super(BBBC010).__init__(
+            image_set="BBBC010",*args,**kwargs
+        )
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -6,3 +6,10 @@ def test_download(dataset):
     data = dataset()
     assert len(data)>0
     assert data[0] is not None
+    
+
+@pytest.mark.parametrize("dataset", ["BBBC010",])
+def test_download(dataset):
+    data = datasets.BroadDataset(dataset)
+    assert len(data)>0
+    assert data[0] is not None
