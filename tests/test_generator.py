d61f7366fd6b576a98202901f9d30c6ca84f8240
Craig <ctr26@ebi.ac.uk>
2022-08-03 09:18:45 +0100

    formatting

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -20,6 +20,7 @@ import pyro
 import pytorch_lightning as pl
 from torch.utils.data import random_split, DataLoader
 import glob
+
 # Note - you must have torchvision installed for this example
 from torchvision import datasets
 from torchvision import transforms
@@ -47,16 +48,25 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 
 from mask_vae.datasets import DatasetGlob
-from mask_vae.transforms import ImagetoDistogram, cropCentroid, DistogramToCoords,CropCentroidPipeline
-from mask_vae.transforms import DistogramToCoords, MaskToDistogramPipeline,AsymmetricDistogramToMaskPipeline
+from mask_vae.transforms import (
+    ImagetoDistogram,
+    cropCentroid,
+    DistogramToCoords,
+    CropCentroidPipeline,
+)
+from mask_vae.transforms import (
+    DistogramToCoords,
+    MaskToDistogramPipeline,
+    AsymmetricDistogramToMaskPipeline,
+)
 from mask_vae.transforms import DistogramToMaskPipeline
 
 from mask_vae.models import AutoEncoder, VAE, VQ_VAE, Mask_VAE
 from mask_vae.lightning import LitAutoEncoderPyro, LitAutoEncoderTorch
 
-interp_size = 128*4
+interp_size = 128 * 4
 
-window_size = 128-32
+window_size = 128 - 32
 batch_size = 32
 num_training_updates = 15000
 
@@ -89,27 +99,29 @@ transformer_coords = DistogramToCoords(window_size)
 train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_dist)
 train_dataset_crop = DatasetGlob(train_dataset_glob, transform=transformer_crop)
 
-assert(len(train_dataset)>0)
-assert(len(train_dataset_crop)>0)
+assert len(train_dataset) > 0
+assert len(train_dataset_crop) > 0
+
 
 def test_glob():
-    assert(len(train_dataset)>0)
-    assert(len(train_dataset_crop)>0)
+    assert len(train_dataset) > 0
+    assert len(train_dataset_crop) > 0
     plt.imshow(test_img[0][0])
     plt.show()
-    
+
+
 # img_squeeze = train_dataset_crop[0].unsqueeze(0)
 # img_crop = train_dataset_crop[0]
 
-dataloader = DataLoader(train_dataset, batch_size=batch_size,
-                        shuffle=True, num_workers=8, pin_memory=True)
+dataloader = DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True
+)
 
 ckpt_file = "checkpoints/last.ckpt"
 
 
 model = Mask_VAE(VQ_VAE(channels=1))
-model = LitAutoEncoderTorch(model).load_from_checkpoint(
-    ckpt_file, model=model)
+model = LitAutoEncoderTorch(model).load_from_checkpoint(ckpt_file, model=model)
 
 test_img = train_dataset[1].unsqueeze(0)
 # img_squeeze = train_dataset_crop[1].unsqueeze(0)
@@ -118,30 +130,37 @@ img_crop = train_dataset_crop[1].unsqueeze(0)
 # train_dataset = train_dataset_dist
 
 
-    
 def test_img_test():
     plt.close()
     plt.imshow(test_img[0][0])
     plt.show()
-   
+
+
 def test_forward_test():
     plt.close()
-    loss,y_prime,_, = model.forward(test_img)
+    (
+        loss,
+        y_prime,
+        _,
+    ) = model.forward(test_img)
     y_prime = y_prime.detach().numpy()
     plt.imshow(y_prime[0][0])
     plt.show()
+
+
 def test_encoder_decoder():
     plt.close()
     z = model.encoder(test_img)
     y_prime = model.decoder(z).detach().numpy()
     plt.imshow(y_prime[0][0])
     plt.show()
-    
+
+
 def test_generate():
     z = model.encoder(test_img)
     z_random = torch.normal(torch.zeros_like(z), torch.ones_like(z))
     # z_random = torch.ones_like(z)
-    z_random = z + torch.normal(torch.zeros_like(z), torch.ones_like(z))/20
+    z_random = z + torch.normal(torch.zeros_like(z), torch.ones_like(z)) / 20
     generated_image_dist = model.decoder(z_random).detach().numpy()
     # dist_stack = np.stack(
     #     [generated_image_dist[0], np.transpose(generated_image_dist[0],)], axis=0)
@@ -161,9 +180,7 @@ def test_generate():
     # coords = DistogramToCoords(window_size)(symmetric_generated_image_dist[0])
     # plt.scatter(coords[0, :, 0], coords[0, :, 1])
     # plt.show()
-    mask = AsymmetricDistogramToMaskPipeline(window_size)(
-        generated_image_dist)
-
+    mask = AsymmetricDistogramToMaskPipeline(window_size)(generated_image_dist)
 
     # generated_image = model.model.mask_from_latent(z_random,window_size)
     # plt.imshow((mask[0]))
@@ -179,4 +196,6 @@ def test_generate():
     plt.imshow(mask[0][0])
     plt.savefig("tests/test_generate_mask.png")
     plt.show()
+
+
 # %%
