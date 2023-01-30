d1012d1c2012b280fa7bd9e5fa507f0ae09b16e8
ctr26 <ctr26@ebi.ac.uk>
2023-01-30 16:20:02 +0000

    pythae compatibility

--- bio_vae/lightning/torch.py
+++ bio_vae/lightning/torch.py
@@ -33,17 +33,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torch.optim as optim
+import pythae
+from pythae.trainers import BaseTrainerConfig, BaseTrainer
 
 
 class LitAutoEncoderTorch(pl.LightningModule):
     def __init__(self, model, batch_size=1, learning_rate=1e-4, params=None):
         super().__init__()
         # self.autoencoder = AutoEncoder(batch_size, 1)
-        self.model = model
+        self.model = model.to(self.device)
+        self.model.train()
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.loss_fn = torch.nn.MSELoss()
         self.params = params
+        self.PYTHAE_FLAG = isinstance(self.model.__class__, type(pythae.models.BaseAE))
         # self.loss_fn = torch.nn.BCEWithLogitsLoss()
         # self.vae = VAE()
         # self.vae_flag = vae_flag
@@ -62,18 +66,24 @@ class LitAutoEncoderTorch(pl.LightningModule):
         return self.model.encode(img)
 
     def forward(self, x):
+        if self.PYTHAE_FLAG:
+            return self.model.forward({"data": x})["recon_x"]
         return self.model(x)
 
     def recon(self, x):
+        if self.PYTHAE_FLAG:
+            return self.forward(x)
         return self.model.recon(x)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
-    def predict_step(self, batch, batch_idx, dataloader_idx=0):
-        return self.recon(batch)
+    # def predict_step(self, batch, batch_idx, dataloader_idx=0):
+    # return self.recon(batch)
 
     def get_loss(self, batch):
+        if self.PYTHAE_FLAG:
+            return self.model.forward({"data": batch})["loss"]
         # self.curr_device = real_img.device
 
         results = self.get_results(batch)
@@ -83,7 +93,7 @@ class LitAutoEncoderTorch(pl.LightningModule):
         return loss["loss"]
 
     def get_results(self, batch):
-        return self.forward(batch)
+        return self.model.forward({"data": batch})
 
     def test_step(self, batch, batch_idx):
         test_loss = self.get_loss(batch)
@@ -106,47 +116,57 @@ class LitAutoEncoderTorch(pl.LightningModule):
         self.logger.experiment.add_image(
             "input", torchvision.utils.make_grid(batch), batch_idx
         )
-        self.logger.experiment.add_image(
-            "output",
-            torchvision.utils.make_grid(self.model.output_from_results(*results)),
-            batch_idx,
-        )
+        if self.PYTHAE_FLAG:
+            self.logger.experiment.add_image(
+                "output",
+                torchvision.utils.make_grid(results["recon_x"]),
+                batch_idx,
+            )
+        else:
+            self.logger.experiment.add_image(
+                "output",
+                torchvision.utils.make_grid(self.model.output_from_results(*results)),
+                batch_idx,
+            )
         return loss
 
-    def _training_step(self, inputs, batch_idx):
-        vq_loss, output, perplexity = self.forward(inputs)
-        # output = x_recon
-        # loss = self.loss_fn(output, inputs)
+    # def _training_step(self, inputs, batch_idx):
+    #     vq_loss, output, perplexity = self.forward(inputs)
+    #     # output = x_recon
+    #     # loss = self.loss_fn(output, inputs)
 
-        # vq_loss, data_recon, perplexity = model(inputs)
-        # recon_error = F.mse_loss(output, inputs)
-        recon_error = self.loss_fn(output, inputs)
-        loss = recon_error + vq_loss  # Missing variance bit
-        self.log("train_loss", loss)
-        # tensorboard = self.logger.experiment
-        # self.logger.experiment.add_scalar("Loss/train", loss, batch_idx)
+    #     # vq_loss, data_recon, perplexity = model(inputs)
+    #     # recon_error = F.mse_loss(output, inputs)
+    #     recon_error = self.loss_fn(output, inputs)
+    #     loss = recon_error + vq_loss  # Missing variance bit
+    #     self.log("train_loss", loss)
+    #     # tensorboard = self.logger.experiment
+    #     # self.logger.experiment.add_scalar("Loss/train", loss, batch_idx)
 
-        # torchvision.utils.make_grid(output)
-        self.logger.experiment.add_image(
-            "input", torchvision.utils.make_grid(inputs), batch_idx
-        )
-        # self.logger.experiment.add_embedding(
-        #     "input_image", torchvision.utils.make_grid(transformer_image(inputs)), batch_idx)
-        self.logger.experiment.add_image(
-            "output", torchvision.utils.make_grid(output), batch_idx
-        )
-        # self.logger.experiment.add_embedding(
-        #     "output_image", torchvision.utils.make_grid(transformer_image(output)), batch_idx)
+    #     # torchvision.utils.make_grid(output)
+    #     self.logger.experiment.add_image(
+    #         "input", torchvision.utils.make_grid(inputs), batch_idx
+    #     )
+    #     # self.logger.experiment.add_embedding(
+    #     #     "input_image", torchvision.utils.make_grid(transformer_image(inputs)), batch_idx)
+    #     self.logger.experiment.add_image(
+    #         "output", torchvision.utils.make_grid(output), batch_idx
+    #     )
+    #     # self.logger.experiment.add_embedding(
+    #     #     "output_image", torchvision.utils.make_grid(transformer_image(output)), batch_idx)
 
-        # tensorboard.add_image("input", transforms.ToPILImage()(output[batch_idx]), batch_idx)
-        # tensorboard.add_image("output", transforms.ToPILImage()(output[batch_idx]), batch_idx)
-        return loss
+    #     # tensorboard.add_image("input", transforms.ToPILImage()(output[batch_idx]), batch_idx)
+    #     # tensorboard.add_image("output", transforms.ToPILImage()(output[batch_idx]), batch_idx)
+    #     return loss
 
     def get_embedding(self):
         return self.model.get_embedding()
 
     def sample(self, *args, **kwargs):
         return self.model.sample(*args, **kwargs)
+
+    def get_model(self):
+        return self.model
 
     @property
     def num_training_steps(self) -> int:
