f94b7e2f80efd2de845ef7b37c52d8ab7829d51a
ctr26 <ctr26@ebi.ac.uk>
2023-02-16 12:19:34 +0000

    pythae class not detecting properly

--- bio_vae/lightning/torch.py
+++ bio_vae/lightning/torch.py
@@ -41,18 +41,25 @@ class LitAutoEncoderTorch(pl.LightningModule):
     def __init__(self, model, batch_size=1, learning_rate=1e-4, params=None):
         super().__init__()
         # self.autoencoder = AutoEncoder(batch_size, 1)
-        self.model = model.to(self.device)
-        self.model.train()
+        
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.loss_fn = torch.nn.MSELoss()
         self.params = params
-        self.PYTHAE_FLAG = isinstance(self.model.__class__, type(pythae.models.BaseAE))
+        self.model = model
+        self.PYTHAE_FLAG = isinstance(self.model.__class__, pythae.models.BaseAE)
+        if self.PYTHAE_FLAG:
+            self.pythae_flag()
         # self.loss_fn = torch.nn.BCEWithLogitsLoss()
         # self.vae = VAE()
         # self.vae_flag = vae_flag
         # self.loss_fn = torch.nn.BCELoss()
-
+        
+    def pythae_flag(self):
+        self.model = self.model.to(self.device)
+        self.model.train()
+        
+        
     def decoder(self, z):
         return self.model.decoder(z)
 
@@ -68,7 +75,7 @@ class LitAutoEncoderTorch(pl.LightningModule):
     def forward(self, x):
         if self.PYTHAE_FLAG:
             return self.model.forward({"data": x})["recon_x"]
-        return self.model(x)
+        return self.model.forward(x)
 
     def recon(self, x):
         if self.PYTHAE_FLAG:
@@ -106,7 +113,6 @@ class LitAutoEncoderTorch(pl.LightningModule):
         return val_loss
 
     def training_step(self, batch, batch_idx, optimizer_idx=0):
-
         loss = self.get_loss(batch)
         results = self.get_results(batch)
 
