81dabcfaad0384290c5ab1d6ecad0c5226f24244
Craig Russell <ctr26@ebi.ac.uk>
2023-04-28 14:47:01 +0100

    Fixed model (residual block was missing)

--- bio_vae/models/vq_vae.py
+++ bio_vae/models/vq_vae.py
@@ -188,27 +188,6 @@ class ResidualStack(nn.Module):
         return F.relu(x)
 
 
-# num_hiddens = 64
-# num_residual_hiddens = 32
-# num_residual_layers = 2
-
-# embedding_dim = 64
-# num_embeddings = 512
-
-# commitment_cost = 0.25
-
-# decay = 0.99
-
-# num_hiddens=64
-# num_residual_hiddens=32
-# num_residual_layers=2
-# embedding_dim=64
-# num_embeddings=512
-# commitment_cost=0.25
-# decay=0.99
-# channels=1
-
-
 class Encoder(BaseEncoder):
     def __init__(
         self,
@@ -243,36 +222,43 @@ class Encoder(BaseEncoder):
         self._conv_3 = nn.Conv2d(
             in_channels=num_hiddens,
             out_channels=num_hiddens,
-            kernel_size=3,
-            stride=1,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+        )
+        self._conv_4 = nn.Conv2d(
+            in_channels=num_hiddens,
+            out_channels=embedding_dim,
+            kernel_size=4,
+            stride=2,
             padding=1,
         )
         self._residual_stack = ResidualStack(
-            in_channels=num_hiddens,
-            num_hiddens=num_hiddens,
-            num_residual_layers=num_residual_layers,
-            num_residual_hiddens=num_residual_hiddens,
-        )
+                                in_channels=num_hiddens,
+                                num_hiddens=num_hiddens,
+                                num_residual_layers=num_residual_layers,
+                                num_residual_hiddens=num_residual_hiddens)
 
-        self._pre_vq_conv = nn.Conv2d(
-            in_channels=num_hiddens, out_channels=embedding_dim, kernel_size=1, stride=1
-        )
+        self._adaptive_pool = nn.AdaptiveAvgPool2d((embedding_dim, embedding_dim))
 
     def forward(self, inputs):
         output = ModelOutput()
-
-        x = self._conv_1(inputs)
+        x = inputs
+        x = self._conv_1(x)
         x = F.relu(x)
 
         x = self._conv_2(x)
         x = F.relu(x)
 
         x = self._conv_3(x)
-        x = self._residual_stack(x)
-        x = self._pre_vq_conv(x)
-        output["embedding"] = x
-        return output
+        x = F.relu(x)
 
+        x = self._conv_4(x)
+        # x = F.relu(x)
+        x = self._residual_stack(x)
+        output["embedding"] = x
+
+        return output
 
 # import pythae
 class Decoder(BaseDecoder):
@@ -293,22 +279,15 @@ class Decoder(BaseDecoder):
             out_channels = model_config.input_dim[0]
             image_dims = model_config.input_dim[1:]
 
-        self._conv_1 = nn.Conv2d(
+        self._conv_trans_1 = nn.ConvTranspose2d(
             in_channels=in_channels,
             out_channels=num_hiddens,
-            kernel_size=3,
-            stride=1,
+            kernel_size=4,
+            stride=2,
             padding=1,
         )
 
-        self._residual_stack = ResidualStack(
-            in_channels=num_hiddens,
-            num_hiddens=num_hiddens,
-            num_residual_layers=num_residual_layers,
-            num_residual_hiddens=num_residual_hiddens,
-        )
-
-        self._conv_trans_1 = nn.ConvTranspose2d(
+        self._conv_trans_2 = nn.ConvTranspose2d(
             in_channels=num_hiddens,
             out_channels=num_hiddens // 2,
             kernel_size=4,
@@ -316,26 +295,49 @@ class Decoder(BaseDecoder):
             padding=1,
         )
 
-        self._conv_trans_2 = nn.ConvTranspose2d(
+        self._conv_trans_3 = nn.ConvTranspose2d(
             in_channels=num_hiddens // 2,
-            out_channels=out_channels,
+            out_channels=num_hiddens // 4,
             kernel_size=4,
             stride=2,
             padding=1,
         )
 
+        self._conv_trans_4 = nn.ConvTranspose2d(
+            in_channels=num_hiddens // 4,
+            out_channels=out_channels,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+        )
+        
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
     def forward(self, inputs):
         output = ModelOutput()
-
-        x = self._conv_1(inputs)
-
+        
+        
+        x = inputs
         x = self._residual_stack(x)
-
+        
+        # x = F.relu(x)
         x = self._conv_trans_1(x)
         x = F.relu(x)
-        output["reconstruction"] = self._conv_trans_2(x)
-        return output
 
+        x = self._conv_trans_2(x)
+        x = F.relu(x)
+
+        x = self._conv_trans_3(x)
+        x = F.relu(x)
+
+        x = self._conv_trans_4(x)
+        x = F.relu(x)
+        
+        output["reconstruction"] = x
+        return output
 
 class VQ_VAE(nn.Module):
     def __init__(
@@ -449,18 +451,9 @@ class VQ_VAE(nn.Module):
         :return:
         """
         vq_loss, recons, perplexity = args
-        # recons = args[0]
-        # input = args[1]
-        # vq_loss = args[2]
+
         recons_loss = F.mse_loss(recons, input)
 
         loss = recons_loss + vq_loss
         return {"loss": loss, "Reconstruction_Loss": recons_loss, "VQ_Loss": vq_loss}
 
-        # vq_loss, output, perplexity = self.forward(inputs)
-        # output = x_recon
-        # loss = self.loss_fn(output, inputs)
-
-        # vq_loss, data_recon, perplexity = model(inputs)
-        # recon_error = F.mse_loss(output, inputs)
-        # recon_error = self.loss_fn(output, inputs)
