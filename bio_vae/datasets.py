a440fba92614431ca544610e924165c19aeb3d01
Craig Russell <craig.russell.phd@gmail.com>
2023-05-02 16:48:12 +0100

    Trying to speed up dataset (slow transform before)

--- bio_vae/datasets.py
+++ bio_vae/datasets.py
@@ -1,41 +1,34 @@
 #  %%
-import sys
-from torch.utils.data import random_split, DataLoader
 import glob
 import random
 
 # Note - you must have torchvision installed for this example
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from PIL import Image
 import os
-from scipy import ndimage
-import matplotlib.pyplot as plt
 import numpy as np
-import torch
-from torch import nn
-from scipy.ndimage import convolve, sobel
-
-from scipy.interpolate import interp1d
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import torch.optim as optim
 from urllib.error import URLError
 
-from bio_vae.idr import IDRDataSet
-from PIL import Image, ImageSequence
+from PIL import Image
 
 from torchvision.datasets.utils import (
     check_integrity,
     download_and_extract_archive,
-    extract_archive,
-    verify_str_arg,
 )
+
+from functools import lru_cache
 
 
 class DatasetGlob(Dataset):
-    def __init__(self, path_glob, transform=None, samples=-1, shuffle=True, **kwargs):
+    def __init__(
+        self,
+        path_glob,
+        over_sampling=1,
+        transform=None,
+        samples=-1,
+        shuffle=True,
+        **kwargs,
+    ):
         self.image_paths = glob.glob(path_glob, recursive=True)
         if shuffle:
             random.shuffle(self.image_paths)
@@ -43,26 +36,29 @@ class DatasetGlob(Dataset):
             self.image_paths = self.image_paths[0:samples]
         self.transform = transform
         self.samples = samples
+        self.over_sampling = over_sampling
         assert len(self.image_paths) > 0
 
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.image_paths) * self.over_sampling
 
-    def getitem(self, index):
-        try:
-            x = Image.open(self.image_paths[index])
-            # x = np.array(x)
-            x = ImageSequence.Iterator(x)
-            if self.transform is not None:
-                x = self.transform(image=np.array(x[0]))["image"]
+    @lru_cache(maxsize=None)
+    def get_cached_image(self, index):
+        return Image.open(self.image_paths[index])
+
+    def getitem(self, index, cached=True):
+        safe_idx = int((index) % (len(self) / self.over_sampling))
+
+        if cached:
+            x = self.get_cached_image(safe_idx)
+        else:
+            x = Image.open(self.image_paths[safe_idx])
+
+        if self.transform is not None:
+            x = self.transform(image=np.array(x))["image"]
             return x
-        except:
-            return None
 
-    # def make_subset(self, index):
-    #     self.getitem(index)
-
-    def is_image_cropped(self,image):
+    def is_image_cropped(self, image):
         if (
             np.sum(
                 np.sum(image[:, 0]),
@@ -76,26 +72,32 @@ class DatasetGlob(Dataset):
         else:
             return True
 
-        # return self.transform(Image.open(self.image_paths[index]))
 
     def __getitem__(self, index):
-        # x = self.getitem(index)
-        # if self.is_image_cropped(x):
-        # return index
-        # if isinstance(index, slice):
-        # return [self.getitem(i) for i in range(*index.indices(10))]
-        # TODO implement indexing/slicing
-        # return self.getitem(index)
-
-        # return self.getitem(index)
-        dummy_list = np.arange(0, self.__len__())
-        loop = np.array(dummy_list[index])
+        x = self.getitem(index)
         if isinstance(index, slice):
-            return [self.getitem(i) for i in loop]
-        return self.getitem(index)
+            return [self.getitem(i) for i in range(*index.indices(len(self)))]
+        else:
+            return x
 
-        # else:
-        #     return self.getitem(index+x)
+    # def __getitem__(self, index):
+    #     # x = self.getitem(index)
+    #     # if self.is_image_cropped(x):
+    #     # return index
+    #     # if isinstance(index, slice):
+    #     # return [self.getitem(i) for i in range(*index.indices(10))]
+    #     # TODO implement indexing/slicing
+    #     # return self.getitem(index)
+
+    #     # return self.getitem(index)
+    #     dummy_list = np.arange(0, self.__len__())
+    #     loop = np.array(dummy_list[index])
+    #     if isinstance(index, slice):
+    #         return [self.getitem(i) for i in loop]
+    #     return self.getitem(index)
+
+    # else:
+    #     return self.getitem(index+x)
 
 
 class WebArchiveDataset(DatasetGlob):
