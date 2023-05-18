ed439ce46bb9c4f1d8c07966a3a4ccb26951c4f1
Craig Russell <craig.russell.phd@gmail.com>
2023-05-18 15:40:35 +0100

    removing pipeline from biobae models

--- bio_vae/models/__init__.py
+++ bio_vae/models/__init__.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 # Note - you must have torchvision installed for this example
 from torch.utils.data import DataLoader
 
-from bio_vae.transforms import DistogramToMaskPipeline
+
 
 # from .ae import AutoEncoder
 
@@ -16,8 +16,6 @@ from .legacy import Bio_VAE
 
 
 from .bolts import ResNet18VAEEncoder, ResNet18VAEDecoder
-
-import pythae
 
 from . import bolts
 from .factory import ModelFactory, create_model, MODELS
