16c3ca5ff0196bb922679c66bf5740cb0576a0a4
Craig Russell <craig.russell.phd@gmail.com>
2023-05-18 15:41:24 +0100

    removing ai datasets (in seperate repo now)

--- bio_vae/datasets.py
+++ bio_vae/datasets.py
@@ -2,19 +2,13 @@
 import glob
 import random
 
-# Note - you must have torchvision installed for this example
 from torch.utils.data import Dataset
 from PIL import Image
-import os
 import numpy as np
-from urllib.error import URLError
 
 from PIL import Image
 
-from torchvision.datasets.utils import (
-    check_integrity,
-    download_and_extract_archive,
-)
+
 
 from functools import lru_cache
 
@@ -99,79 +93,3 @@ class DatasetGlob(Dataset):
 
     # else:
     #     return self.getitem(index+x)
-
-
-class WebArchiveDataset(DatasetGlob):
-    def __init__(
-        self,
-        url,
-        md5,
-        filename,
-        dataset,
-        data_folder="data",
-        download=True,
-        transform=None,
-        **kwargs,
-    ):
-        self.url = url
-        self.md5 = md5
-        self.dataset = dataset
-        self.raw_folder = f"{data_folder}/{self.dataset}"
-        self.filename = filename
-        self.images_file = self.filename
-        path_glob = f"{self.raw_folder}/**/*.png"
-
-        # self.image_paths = glob.glob(path_glob, recursive=True)
-        # self.transform = transform
-        if download:
-            self.download()
-        super(WebArchiveDataset, self).__init__(path_glob, transform, **kwargs)
-
-    def _check_exists(self) -> bool:
-        return all(check_integrity(file) for file in (self.images_file))
-
-    def download(self) -> None:
-
-        if self._check_exists():
-            return
-
-        os.makedirs(self.raw_folder, exist_ok=True)
-
-        # download files
-
-        try:
-            print(f"Downloading {self.url}")
-            download_and_extract_archive(
-                self.url,
-                download_root=self.raw_folder,
-                filename=self.filename,
-                md5=self.md5,
-            )
-        except URLError as error:
-            print(f"Failed to download (trying next):\n{error}")
-
-
-class BroadDataset(WebArchiveDataset):
-    lookup_info = {
-        "BBBC010": {
-            "url": "https://data.broadinstitute.org/bbbc/BBBC010/BBBC010_v1_foreground_eachworm.zip",
-            "md5": "ae73910ed293397b4ea5082883d278b1",
-            "dataset": "bbbc010",
-            "filename": "BBBC010_v1_foreground_eachworm.zip",
-        }
-    }
-
-    def __init__(
-        self,
-        image_set="BBBC010",
-        **kwargs,
-    ):
-        super(BroadDataset, self).__init__(
-            **self.lookup_info[image_set],
-            **kwargs,
-        )
-
-
-class BBBC010(BroadDataset):
-    def __init__(self, *args, **kwargs):
-        super(BBBC010).__init__(image_set="BBBC010", *args, **kwargs)
