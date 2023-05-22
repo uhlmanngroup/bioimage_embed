d0f46de3fff6f5673e13d0bf5921dc8a79286c4f
Craig Russell <craig.russell.phd@gmail.com>
2023-05-22 11:41:31 +0100

    updating the legacy script (hopefully will be outdated soon in favour of cconfigs)

--- scripts/train_ivy_gap_legacy.py
+++ scripts/train_ivy_gap_legacy.py
@@ -20,8 +20,7 @@ from bio_vae.lightning import DatamoduleGlob, LitAutoEncoderTorch
 from types import SimpleNamespace
 
 # import timm
-# from bio_vae.models import VQ_VAE, Bio_VAE
-from bio_vae.models.legacy import Bio_VAE
+from bio_vae.models.legacy import Bio_VAE, VQ_VAE
 
 Image.MAX_IMAGE_PIXELS = None
 
@@ -34,10 +33,18 @@ params = {
     "epochs": 500,
     "batch_size": 2**4,
     "num_workers": 2**4,
-    "window_size": 64 * 2,
-    "channels": 3,
-    "latent_dim": 64,
-    "num_embeddings": 512,
+    # "window_size": 64*2,
+    "input_dim": (3,128,128),
+    # "channels": 3,
+    "latent_dim": 8,
+    "num_embeddings": 8,
+    "num_hiddens": 8,
+    "num_residual_hiddens": 150,
+    "num_residual_layers":8 ,
+    # "embedding_dim": 32,
+    # "num_embeddings": 16,
+    "commitment_cost":0.25,
+    "decay":0.99,
 }
 
 optimizer_params = {
@@ -49,27 +56,33 @@ optimizer_params = {
 
 lr_scheduler_params = {
     "sched": "cosine",
-    "min_lr": 1e-6,
+    "min_lr": 1e-4,
     "warmup_epochs": 5,
     "warmup_lr": 1e-6,
     "cooldown_epochs": 10,
     "t_max": 50,
     "cycle_momentum": False,
 }
+
+# channels = 3
+
+# input_dim = (params["channels"], params["window_size"], params["window_size"])
 args = SimpleNamespace(**params, **optimizer_params, **lr_scheduler_params)
 
 # num_embeddings = 128
 # decay = 0.99
 # learning_rate = 1e-2
 
-channels = 3
-
-input_dim = (args.channels, args.window_size, args.window_size)
 # latent_dim = 64
 
 
 transform = A.Compose(
     [
+        # A.RandomCrop(
+        #     height=512,
+        #     width=512,
+        #     p=1,
+        # ),
         # Flip the images horizontally or vertically with a 50% chance
         A.OneOf(
             [
@@ -78,33 +91,33 @@ transform = A.Compose(
             ],
             p=0.5,
         ),
-        # Rotate the images by a random angle within a specified range
-        A.Rotate(limit=45, p=0.5),
-        # Randomly scale the image intensity to adjust brightness and contrast
-        A.RandomGamma(gamma_limit=(80, 120), p=0.5),
-        # Apply random elastic transformations to the images
-        A.ElasticTransform(
-            alpha=1,
-            sigma=50,
-            alpha_affine=50,
-            p=0.5,
-        ),
-        # Shift the image channels along the intensity axis
-        # Add a small amount of noise to the images
-        A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
-        # Crop a random part of the image and resize it back to the original size
+        # # Rotate the images by a random angle within a specified range
+        # A.Rotate(limit=45, p=0.5),
+        # # Randomly scale the image intensity to adjust brightness and contrast
+        # A.RandomGamma(gamma_limit=(80, 120), p=0.5),
+        # # Apply random elastic transformations to the images
+        # A.ElasticTransform(
+        #     alpha=1,
+        #     sigma=50,
+        #     alpha_affine=50,
+        #     p=0.5,
+        # ),
+        # # Shift the image channels along the intensity axis
+        # # Add a small amount of noise to the images
         A.RandomCrop(
-            height=args.window_size,
-            width=args.window_size,
+            height=args.input_dim[1],
+            width=args.input_dim[2],
             p=1,
         ),
-        A.ToFloat(max_value=1, p=1.0),
+        # # A.ToFloat(max_value=1, p=1.0),
+        A.Normalize(mean=0, std=1, p=1.0),
         ToTensorV2(),
     ]
 )
 
 # train_dataset = DatasetGlob(train_dataset_glob)
 train_dataset = DatasetGlob(train_dataset_glob, transform=transform)
+
 # train_dataset = DatasetGlob(train_dataset_glob )
 
 # plt.imshow(train_dataset[100][0], cmap="gray")
@@ -120,19 +133,44 @@ dataloader = DatamoduleGlob(
     transform=transform,
     pin_memory=True,
     persistent_workers=True,
-    over_sampling=16,
+    over_sampling=1,
 )
 
 
 model_config_vqvae = pythae.models.VQVAEConfig(
-    input_dim=input_dim, latent_dim=args.latent_dim, num_embeddings=args.num_embeddings
+**vars(args),
 )
+import bio_vae
+# model = VQ_VAE(model_config=model_config_vqvae, channels=args.channels)
+model = bio_vae.models.pythae.legacy.vq_vae.VQVAE(model_config=model_config_vqvae,**vars(args))
+# model = bio_vae.models.create_model("resnet18_vqvae", input_dim, args.latent_dim)
+
+# import pythae
+
+# from pythae.models.nn.benchmarks.celeba import (
+#     Encoder_ResNet_VQVAE_CELEBA,
+#     Encoder_ResNet_VAE_CELEBA,
+#     Decoder_ResNet_VQVAE_CELEBA,
+#     Decoder_ResNet_AE_CELEBA,
+# )
+
+# config = pythae.models.VAEConfig(input_dim=input_dim, latent_dim=args.latent_dim)
+
+# model = pythae.models.VAE(
+#     config,
+#     encoder=Encoder_ResNet_VAE_CELEBA(config),
+#     decoder=Decoder_ResNet_AE_CELEBA(config),
+# )
+
+# config = pythae.models.VQVAEConfig(input_dim=input_dim, latent_dim=args.latent_dim)
+
+# model = pythae.models.VQVAE(
+#     config,
+#     encoder=Encoder_ResNet_VQVAE_CELEBA(config),
+#     decoder=Decoder_ResNet_VQVAE_CELEBA(config),
+# )
 
 
-model = Bio_VAE("VQ_VAE", model_config=model_config_vqvae, channels=args.channels)
-
-from bio_vae.models import create_model
-model = create_model("resnet18_vae", input_dim=input_dim, latent_dim=args.latent_dim)
 lit_model = LitAutoEncoderTorch(model, args)
 
 # %%
@@ -171,18 +209,18 @@ model.eval()
 
 
 model_name = model._get_name()
-model_dir = f"models/{dataset}_{model_name}"
+model_dir = f"my_models/{dataset}_{model_name}"
 
-# %%
-lit_model = LitAutoEncoderTorch(
-    model,
-    args
-    # learning_rate=learning_rate,
-    # optimizer_params=optimizer_params,
-    # lr_scheduler_params=lr_scheduler_params,
-)
+# # %%
+# lit_model = LitAutoEncoderTorch(
+#     model,
+#     args
+#     # learning_rate=learning_rate,
+#     # optimizer_params=optimizer_params,
+#     # lr_scheduler_params=lr_scheduler_params,
+# )
 
-tb_logger = pl_loggers.TensorBoardLogger(f"{model_dir}/runs/")
+tb_logger = pl_loggers.TensorBoardLogger(f"logs/")
 
 Path(f"{model_dir}/").mkdir(parents=True, exist_ok=True)
 
