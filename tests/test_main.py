c0eff2fa97f7519372b2d50b458fb889554442fe
Craig Russell <craig.russell.phd@gmail.com>
2023-06-16 13:24:44 +0100

    Fixing tests adding latent space test

--- tests/test_main.py
+++ tests/test_main.py
@@ -27,37 +27,41 @@ from bio_vae.models import MODELS
 # from bio_vae.models import VQ_VAE, Bio_VAE, VAE
 from bio_vae.lightning import LitAutoEncoderTorch
 from bio_vae.models.legacy import vae
+import  numpy as np
+from bio_vae.models import create_model, MODELS
 
 
 # @pytest.mark.skip(reason="Crashes github actions")
-# models = [vq_vae.VQ_VAE, vae.VAE]
-# dataset = [torch.randn(1, 3, 64, 64)]
-
 input_dim = [(3,64,64),(1,64,64)]
-latent_dim = [16]
-
-from bio_vae.models import create_model, MODELS
+latent_dim = np.power(2,np.arange(2,5))
 
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("input_dim", input_dim)
 @pytest.mark.parametrize("latent_dim", latent_dim)
+class TestModels:
+    def test_training(self,model, input_dim, latent_dim):
+        # model = wrapper(model)
+        dataset = torch.rand(1,*input_dim)
+        model = create_model(model, input_dim=input_dim, latent_dim=latent_dim)
+        lit_model = LitAutoEncoderTorch(model)
+        dataloader = DataLoader(
+            dataset,
+            batch_size=1,
+            shuffle=True,
+            num_workers=1,
+            pin_memory=False,
+            collate_fn=collate_none,
+        )
+        trainer = pl.Trainer(
+            max_steps=1,
+            max_epochs=1,
+        )  # .from_argparse_args(args)
+        # trainer.test(lit_model, dataloader)
+        trainer.fit(lit_model, dataloader)
 
-def test_training(model, input_dim, latent_dim):
-    # model = wrapper(model)
-    dataset = torch.rand(1,*input_dim)
-    model = create_model(model, input_dim=input_dim, latent_dim=latent_dim)
-    lit_model = LitAutoEncoderTorch(model)
-    dataloader = DataLoader(
-        dataset,
-        batch_size=1,
-        shuffle=True,
-        num_workers=1,
-        pin_memory=False,
-        collate_fn=collate_none,
-    )
-    trainer = pl.Trainer(
-        max_steps=1,
-        max_epochs=1,
-    )  # .from_argparse_args(args)
-    # trainer.test(lit_model, dataloader)
-    trainer.fit(lit_model, dataloader)
+    def test_latent_space(self,model,input_dim,latent_dim):
+        batch_size = 1
+        dataset = torch.rand(batch_size,*input_dim)
+        model = create_model(model, input_dim=input_dim, latent_dim=latent_dim)
+        z = model({"data":dataset})["z"]
+        assert tuple(z.shape) == (batch_size,latent_dim)
