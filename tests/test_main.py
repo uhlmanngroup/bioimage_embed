d235f752605522dd72b3760e32989f29c8984487
Craig Russell <craig.russell.phd@gmail.com>
2023-06-17 20:22:51 +0100

    Adding a lot of model tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -34,19 +34,20 @@ from bio_vae.models import create_model, MODELS
 # @pytest.mark.skip(reason="Crashes github actions")
 input_dim = [(3,64,64),(1,64,64)]
 latent_dim = np.power(2,np.arange(2,5))
-
+batch_size = [1,2]
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("input_dim", input_dim)
 @pytest.mark.parametrize("latent_dim", latent_dim)
+@pytest.mark.parametrize("batch_size", batch_size)
 class TestModels:
-    def test_training(self,model, input_dim, latent_dim):
+    def test_training(self,model, input_dim, latent_dim,batch_size):
         # model = wrapper(model)
-        dataset = torch.rand(1,*input_dim)
+        dataset = torch.rand(batch_size,*input_dim)
         model = create_model(model, input_dim=input_dim, latent_dim=latent_dim)
         lit_model = LitAutoEncoderTorch(model)
         dataloader = DataLoader(
             dataset,
-            batch_size=1,
+            batch_size=batch_size,
             shuffle=True,
             num_workers=1,
             pin_memory=False,
@@ -59,8 +60,7 @@ class TestModels:
         # trainer.test(lit_model, dataloader)
         trainer.fit(lit_model, dataloader)
 
-    def test_latent_space(self,model,input_dim,latent_dim):
-        batch_size = 1
+    def test_latent_space(self,model,input_dim,latent_dim,batch_size):
         dataset = torch.rand(batch_size,*input_dim)
         model = create_model(model, input_dim=input_dim, latent_dim=latent_dim)
         z = model({"data":dataset})["z"]
