9b54f30b30c2e0020f2b4f7eae69191685b5560a
Craig Russell <craig.russell.phd@gmail.com>
2023-06-30 17:12:24 +0100

    standardised functions for override

--- bio_vae/shapes/lightning.py
+++ bio_vae/shapes/lightning.py
@@ -13,15 +13,25 @@ def diagonal_loss(distance_matrix):
     )
 
 def symmetry_loss(distance_matrix):
-    return F.mse_loss(distance_matrix, distance_matrix.transpose(3, 2))
+    return F.mse_loss(distance_matrix, distance_matrix.transpose(-2, -1))
+
+# def symmetry_loss(distance_matrix):
+#     return F.mse_loss(distance_matrix, torch.transpose(distance_matrix, dim0=-2, dim1=-1))
 
 
-class Mask_Embed(LitAutoEncoderTorch):
+
+class MaskEmbed(LitAutoEncoderTorch):
     def __init__(self, model,args=None):
-        super(LitAutoEncoderTorch).__init__()
-
+        super().__init__(model,args)
+    def batch_to_tensor(self, batch):
+        return super().batch_to_tensor(batch[0].float())
+    
     def loss_function(self, model_output, *args, **kwargs):
         loss = model_output.loss
         loss = loss + diagonal_loss(model_output.recon_x)
         loss = loss + symmetry_loss(model_output.recon_x)
         return loss
+    
+
+    # def embedding_from_output(self,model_output):
+    #     return model_output.z.view(model_output.z.shape[0], -1)
