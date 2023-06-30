62f0cae6384605faec0a1c0b2e16112bfc25c0b3
Craig Russell <craig.russell.phd@gmail.com>
2023-06-30 17:13:15 +0100

    Large refactor of mask script

--- scripts/train_mask.py
+++ scripts/train_mask.py
@@ -2,6 +2,9 @@
 import os
 from pathlib import Path
 
+from torch.autograd import Variable
+import torchvision
+
 #  %%
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 import pytorch_lightning as pl
@@ -12,8 +15,9 @@ from torch.utils.data import DataLoader
 import os
 from pytorch_lightning import loggers as pl_loggers
 from torchvision import transforms
-from bio_vae.lightning import DatamoduleGlob
+from bio_vae.lightning import DataModuleGlob, DataModule
 
+from torchvision import datasets
 from bio_vae.datasets import DatasetGlob
 from bio_vae.shapes.transforms import (
     CropCentroidPipeline,
@@ -21,37 +25,73 @@ from bio_vae.shapes.transforms import (
     DistogramToCoords,
     MaskToDistogramPipeline,
 )
-from bio_vae.models import Mask_VAE, VQ_VAE, VAE
+
+# from bio_vae.models import Mask_VAE, VQ_VAE, VAE
 from bio_vae.lightning import LitAutoEncoderTorch, LitAutoEncoderPyro
 import matplotlib.pyplot as plt
 
-interp_size = 128 * 4
+from bio_vae.lightning import DataModule, DataModuleGlob, LitAutoEncoderTorch
+import matplotlib
+matplotlib.use('TkAgg')
+interp_size = 128 *2
 
 max_epochs = 500
 
-window_size = 128 * 4
-batch_size = 2
-num_training_updates = 15000
+window_size = 128*2
 
-num_hiddens = 64
-num_residual_hiddens = 32
-num_residual_layers = 2
 
-embedding_dim = 64
-num_embeddings = 512
+params = {
+    "epochs": 500,
+    "batch_size": 4,
+    "num_workers": 2**4,
+    # "window_size": 64*2,
+    "num_workers": 1,
+    "input_dim": (1, window_size, window_size),
+    # "channels": 3,
+    "latent_dim": 16,
+    "num_embeddings": 16,
+    "num_hiddens": 16,
+    "num_residual_hiddens": 32,
+    "num_residual_layers": 150,
+    # "embedding_dim": 32,
+    # "num_embeddings": 16,
+    "commitment_cost": 0.25,
+    "decay": 0.99,
+}
 
-commitment_cost = 0.25
+optimizer_params = {
+    "opt": "LAMB",
+    "lr": 0.001,
+    "weight_decay": 0.0001,
+    "momentum": 0.9,
+}
 
-decay = 0.99
+lr_scheduler_params = {
+    "sched": "cosine",
+    "min_lr": 1e-4,
+    "warmup_epochs": 5,
+    "warmup_lr": 1e-6,
+    "cooldown_epochs": 10,
+    "t_max": 50,
+    "cycle_momentum": False,
+}
 
-learning_rate = 1e-3
+# channels = 3
+import argparse 
+from types import SimpleNamespace
+# input_dim = (params["channels"], params["window_size"], params["window_size"])
+args = SimpleNamespace(**params, **optimizer_params, **lr_scheduler_params)
 
-dataset = "BBBC010_v1_foreground_eachworm"
-model_name = "VQ_VAE"
+dataset = "bbbc010/BBBC010_v1_foreground_eachworm"
+dataset = "bbbc010"
+model_name = "vqvae"
+
+train_data_path = f"data/{dataset}"
+
 
 # train_dataset_glob = "data-science-bowl-2018/stage1_train/*/masks/*.png"
-train_dataset_glob = "data/stage1_train/*/masks/*.png"
-train_dataset_glob = f"data/{dataset}/*.png"
+# train_dataset_glob = "data/stage1_train/*/masks/*.png"
+# train_dataset_glob = f"data/{dataset}/*.png"
 # %%
 # train_dataset_glob = os.path.join("data/BBBC010_v1_foreground_eachworm/*.png")
 
@@ -65,40 +105,72 @@ train_dataset_glob = f"data/{dataset}/*.png"
 model_dir = f"models/{dataset}_{model_name}"
 # %%
 
-transformer_crop = CropCentroidPipeline(window_size)
-transformer_dist = MaskToDistogramPipeline(window_size, interp_size)
-transformer_coords = DistogramToCoords(window_size)
+transform_crop = CropCentroidPipeline(window_size)
+transform_dist = MaskToDistogramPipeline(window_size, interp_size)
+transform_coords = DistogramToCoords(window_size)
 
-train_dataset = DatasetGlob(train_dataset_glob)
-# train_dataset_crop = DatasetGlob(
-#     train_dataset_glob, transform=CropCentroidPipeline(window_size))
+transform_mask_to_gray = transforms.Compose([transforms.Grayscale(1)])
 
-
-transform = transforms.Compose(
+transform_mask_to_crop = transforms.Compose(
     [
-        transformer_crop,
-        transformer_dist,
+        # transforms.ToTensor(),
+        transform_mask_to_gray,
+        transform_crop,
     ]
 )
 
-train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_dist)
-plt.imshow(train_dataset[0][0], cmap="gray")
-plt.show()
+transform_mask_to_dist = transforms.Compose(
+    [
+        transform_mask_to_crop,
+        transform_dist,
+    ]
+)
+transform_mask_to_coords = transforms.Compose(
+    [
+        transform_mask_to_dist,
+        # transform_coords,
+    ]
+)
 
-train_dataset = DatasetGlob(train_dataset_glob, transform=transforms.ToTensor())
-plt.imshow(train_dataset[0][0], cmap="gray")
-plt.show()
-train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_crop)
-plt.imshow(train_dataset[0][0], cmap="gray")
-plt.show()
+# train_data = torchvision.datasets.ImageFolder(
+# "/home/ctr26/gdrive/+projects/idr_autoencode_torch/data/bbbc010"
+# )
+# train_dataset_crop = DatasetGlob(
+#     train_dataset_glob, transform=CropCentroidPipeline(window_size))
+transforms_dict = {
+    "none": transform_mask_to_gray,
+    "transform_crop": transform_mask_to_crop,
+    "transform_dist": transform_mask_to_dist,
+    # "transform_coords": transform_mask_to_coords,
+}
 
-train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_crop)
-plt.imshow(train_dataset[0][0], cmap="gray")
-plt.show()
+train_data = {
+    key: datasets.ImageFolder(train_data_path, transform=value)
+    for key, value in transforms_dict.items()
+}
 
-train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_dist)
-plt.imshow(train_dataset[0][0], cmap="gray")
-plt.show()
+for key, value in train_data.items():
+    print(key, len(value))
+    plt.imshow(train_data[key][0][0], cmap="gray")
+    plt.imsave(f"{key}.png", train_data[key][0][0], cmap="gray")
+    # plt.show()
+    plt.close()
+
+
+# train_dataset = DatasetGlob(train_data, transform=transforms.ToTensor())
+# plt.imshow(train_dataset[0][0], cmap="gray")
+# plt.show()
+# train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_crop)
+# plt.imshow(train_dataset[0][0], cmap="gray")
+# plt.show()
+
+# train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_crop)
+# plt.imshow(train_dataset[0][0], cmap="gray")
+# plt.show()
+
+# train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_dist)
+# plt.imshow(train_dataset[0][0], cmap="gray")
+# plt.show()
 
 
 # img_squeeze = train_dataset[0].unsqueeze(0)
@@ -109,27 +181,43 @@ plt.show()
 #     batch = list(filter(lambda x: x is not None, batch))
 #     return torch.utils.data.dataloader.default_collate(batch)
 
-dataloader = DatamoduleGlob(
-    train_dataset_glob,
-    batch_size=batch_size,
+transform = transforms.Compose(
+    [transform_mask_to_dist, transforms.ToTensor()]
+)
+
+dataloader = DataModule(
+    datasets.ImageFolder(train_data_path, transform=transform),
+    batch_size=args.batch_size,
     shuffle=True,
-    num_workers=2**4,
-    transform=transformer_dist,
+    num_workers=args.num_workers,
+    # transform=transform,
 )
 
 # dataloader = DataLoader(train_dataset, batch_size=batch_size,
 #                         shuffle=True, num_workers=2**4, pin_memory=True, collate_fn=my_collate)
+import bio_vae
 
-model = Mask_VAE("VQ_VAE", channels=1)
+model = bio_vae.models.create_model("resnet50_vqvae_legacy", **vars(args))
+from bio_vae import shapes
+
+lit_model = shapes.MaskEmbed(model,args)
 # model = Mask_VAE("VAE", 1, 64,
 #                      #  hidden_dims=[32, 64],
 #                      image_dims=(interp_size, interp_size))
 
 # model = Mask_VAE(VAE())
 # %%
-lit_model = LitAutoEncoderTorch(model)
+# lit_model = LitAutoEncoderTorch(model)
 
-tb_logger = pl_loggers.TensorBoardLogger("{model_dir}/runs/")
+dataloader.setup()
+model.eval()
+# %%
+
+
+model_name = model._get_name()
+model_dir = f"my_models/{dataset}_{model_name}"
+
+tb_logger = pl_loggers.TensorBoardLogger(f"logs/")
 
 Path(f"{model_dir}/").mkdir(parents=True, exist_ok=True)
 
@@ -137,18 +225,32 @@ checkpoint_callback = ModelCheckpoint(dirpath=f"{model_dir}/", save_last=True)
 
 trainer = pl.Trainer(
     logger=tb_logger,
+    gradient_clip_val=0.5,
     enable_checkpointing=True,
-    gpus=1,
-    accumulate_grad_batches=1,
+    devices="auto",
+    accelerator="gpu",
+    accumulate_grad_batches=4,
     callbacks=[checkpoint_callback],
     min_epochs=50,
-    max_epochs=max_epochs,
+    max_epochs=args.epochs,
 )  # .from_argparse_args(args)
 
 # %%
+
 try:
     trainer.fit(lit_model, datamodule=dataloader, ckpt_path=f"{model_dir}/last.ckpt")
 except:
     trainer.fit(lit_model, datamodule=dataloader)
 
+model.eval()
+
+validation = trainer.validate(lit_model, datamodule=dataloader)
+# testing = trainer.test(lit_model, datamodule=dataloader)
+example_input = Variable(torch.rand(1, *args.input_dim))
+
+torch.jit.save(model.to_torchscript(), "model.pt")
+torch.onnx.export(model, example_input, f"{model_dir}/model.onnx")
+
 # %%
+# Inference
+
