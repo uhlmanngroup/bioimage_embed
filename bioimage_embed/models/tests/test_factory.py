f36880f38d3f5a1c3793f14cf42d0cd552257758
Craig Russell <craig.russell.phd@gmail.com>
2023-11-28 13:53:08 +0000

    Messed up the tests

--- bioimage_embed/models/tests/test_factory.py
+++ bioimage_embed/models/tests/test_factory.py
@@ -8,7 +8,7 @@ from bioimage_embed.models import MODELS
 
 # input_dim = [(3, 256, 256), (3, 224, 224),(1, 224, 224)]
 image_dim = [(256, 256), (224, 224)]
-channel_dim = [3,1]
+channel_dim = [3,]
 latent_dim = [64, 16]
 pretrained_options = [True, False]
 progress_options = [True, False]
@@ -21,8 +21,9 @@ progress_options = [True, False]
 @pytest.mark.parametrize("pretrained", pretrained_options)
 @pytest.mark.parametrize("progress", progress_options)
 def test_create_model(model, c,idim, ld, pretrained, progress):
-    generated_model = create_model(model, idim, ld, pretrained, progress)
-    data = torch.rand(1, c, *idim)
+    input_dim = (c, *idim)
+    generated_model = create_model(model, input_dim, ld, pretrained, progress)
+    data = torch.rand(1, *input_dim)
     output = generated_model({"data": data})
     assert output["z"].shape[1] == ld
     assert output["recon_x"].shape == data.shape
