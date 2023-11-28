00e8ca413daecf129dbb752e458ff6aeffe9d0a2
Craig Russell <craig.russell.phd@gmail.com>
2023-11-28 17:54:34 +0000

    Fixed (all?) the tests

--- bioimage_embed/shapes/tests/test_generator.py
+++ bioimage_embed/shapes/tests/test_generator.py
@@ -1,13 +1,14 @@
 #  %%
+
+import pytest
+import torch
+import torchvision.transforms as T
+from torchvision.datasets import VOCSegmentation
+import bioimage_embed.models
 import bioimage_embed
 import torch
 import matplotlib.pyplot as plt
-
-
-#  %%
-
-# Note - you must have torchvision installed for this example
-import os
+import torchvision.transforms as T
 import matplotlib.pyplot as plt
 import torch
 
@@ -22,109 +23,138 @@ from bioimage_embed.shapes.transforms import (
 )
 
 
-interp_size = 128 * 4
-window_size = 128 - 32
-
-# train_dataset_glob = "data/stage1_train/*/masks/*.png"
-# train_dataset_glob = "data/BBBC010_v1_foreground_eachworm/*.png"
-
-# test_dataloader_glob=os.path.join(os.path.expanduser("~"),
-# "data-science-bowl-2018/stage1_test/*/masks/*.png")
-
-transformer_crop = CropCentroidPipeline(window_size)
-transformer_dist = MaskToDistogramPipeline(window_size, interp_size)
-transformer_coords = DistogramToCoords(window_size)
-
-# train_dataset_raw = DatasetGlob(train_dataset_glob)
-# train_dataset_crop = DatasetGlob(
-#     train_dataset_glob, transform=CropCentroidPipeline(window_size))
-# train_dataset = DatasetGlob(train_dataset_glob, transform=transformer_dist)
-# train_dataset_crop = DatasetGlob(train_dataset_glob, transform=transformer_crop)
-train_dataset = None
-transformer_dist = None
-train_dataset_crop = None
-# transformer_crop)
+@pytest.fixture(scope="session")
+def gray2rgb():
+    return T.Lambda(lambda x: x.repeat(3, 1, 1))
 
 
-# img_squeeze = train_dataset_crop[0].unsqueeze(0)
-# img_crop = train_dataset_crop[0]
+@pytest.fixture(scope="session")
+def window_size():
+    return 128 - 32
 
 
-# test_img = train_dataset[1].unsqueeze(0)
-# # img_squeeze = train_dataset_crop[1].unsqueeze(0)
-# img_crop = train_dataset_crop[1].unsqueeze(0)
-
-# train_dataset = train_dataset_dist
-
-input_dim = (3, window_size, window_size)
-latent_dim = 16
-model = bioimage_embed.models.create_model(
-    "resnet18_vqvae_legacy", input_dim, latent_dim, pretrained=False, progress=False
-)
+@pytest.fixture(scope="session")
+def interp_size(window_size):
+    return window_size * 4
 
 
-def test_img_test():
-    plt.close()
-    plt.imshow(test_img[0][0])
-    plt.close()
+@pytest.fixture(scope="session")
+def input_dim(window_size):
+    return (3, window_size, window_size)
 
 
-def test_forward_test():
-    plt.close()
-    results = model.forward(test_img)
-    # y_prime = y_prime.detach().numpy()
-    # plt.imshow(y_prime[0][0])
-    # plt.close()
+@pytest.fixture(scope="session")
+def latent_dim():
+    return 16
 
 
-def test_encoder_decoder():
-    plt.close()
-    z = model.encoder(test_img)
-    y_prime = model.decoder(z).detach().numpy()
-    plt.imshow(y_prime[0][0])
-    plt.close()
+@pytest.fixture(scope="session")
+def model(input_dim, latent_dim):
+    return bioimage_embed.models.create_model(
+        "resnet18_vqvae", input_dim, latent_dim, pretrained=False, progress=False
+    )
 
 
-def test_generate():
-    z = model.encoder(test_img)
-    z_random = torch.normal(torch.zeros_like(z), torch.ones_like(z))
-    # z_random = torch.ones_like(z)
-    z_random = z + torch.normal(torch.zeros_like(z), torch.ones_like(z)) / 20
-    generated_image_dist = model.decoder(z_random).detach().numpy()
-    # dist_stack = np.stack(
-    #     [generated_image_dist[0], np.transpose(generated_image_dist[0],)], axis=0)
-    # dist_stack = np.stack(
-    #     [generated_image_dist, generated_image_dist.transpose(0,1, 3, 2)], axis=0)
+@pytest.fixture(scope="session")
+def voc_dataset(window_size):
+    voc_transform = T.Compose([T.ToTensor()])
+    dataset = VOCSegmentation(
+        root="/tmp",
+        year="2012",
+        image_set="train",
+        download=True,
+        transform=voc_transform,
+        target_transform=voc_transform,
+    )
+    return dataset
 
-    # symmetric_generated_image_dist = np.max(dist_stack, axis=0)
-    # print(symmetric_generated_image_dist.shape)
-    # plt.imshow(symmetric_generated_image_dist[0][0])
-    # plt.savefig("test_dist.png")
-    # plt.close()
 
-    # plt.imshow(symmetric_generated_image_dist[0][0])
-    # plt.savefig("test_dist.png")
-    # plt.close()
+@pytest.fixture(scope="session")
+def binary_mask_tensor(voc_dataset):
+    _, mask = voc_dataset[0]
+    return mask[0]
 
-    # coords = DistogramToCoords(window_size)(symmetric_generated_image_dist[0])
-    # plt.scatter(coords[0, :, 0], coords[0, :, 1])
-    # plt.close()
-    mask = AsymmetricDistogramToMaskPipeline(window_size)(generated_image_dist)
 
-    # generated_image = model.model.mask_from_latent(z_random,window_size)
-    # plt.imshow((mask[0]))
-    # plt.savefig("test_dist_mask.png")
-    # plt.close()
+@pytest.fixture(scope="session")
+def binary_mask(binary_mask_tensor):
+    return T.ToPILImage()(binary_mask_tensor)
 
-    # plt.imshow(train_dataset_crop[0][0])
-    # plt.close()
-    plt.close()
-    plt.imshow(img_crop[0][0])
+
+@pytest.fixture(scope="session")
+def transformer_crop(window_size):
+    return CropCentroidPipeline(window_size)
+
+
+@pytest.fixture(scope="session")
+def transformer_dist(window_size, interp_size):
+    return MaskToDistogramPipeline(window_size, interp_size)
+
+
+@pytest.fixture(scope="session")
+def transformer_coords(window_size):
+    return DistogramToCoords(window_size)
+
+
+@pytest.fixture(scope="session")
+def transform(transformer_crop, transformer_dist, binary_mask):
+    gray2rgb = T.Lambda(lambda x: x.repeat(3, 1, 1))
+    transformer = T.Compose(
+        [
+            T.Grayscale(1),
+            transformer_crop,
+            transformer_dist,
+            # transformer_coords,
+            T.ToTensor(),
+            gray2rgb,
+        ]
+    )
+    return transformer
+
+
+@pytest.fixture(scope="session")
+def distance_matrix(transform, binary_mask):
+    return transform(binary_mask)
+
+@pytest.fixture(scope="session")
+def distance_matrix_tensor(distance_matrix):
+    return distance_matrix.unsqueeze(0).float()
+
+def test_decoder(
+    model,
+    distance_matrix_tensor,
+    window_size,
+):
+    # Test the decoder part of the model
+    output_e = model.encoder(distance_matrix_tensor)
+    output_d = model.decoder(output_e.embedding)
+    y_prime = output_d.reconstruction
+    assert y_prime.shape == (
+        1,
+        3,
+        window_size,
+        window_size,
+    ), "Decoder output shape mismatch"
+
+
+def test_generate(model, binary_mask_tensor, distance_matrix_tensor, window_size):
+    # Test the generate functionality
+    z = model.encoder(distance_matrix_tensor)
+    z_random = torch.normal(2*(torch.ones_like(z.embedding,)))
+    generated_image_dist = model.decoder(z_random)
+    np_dist = generated_image_dist.reconstruction.detach().numpy()
+    mask = AsymmetricDistogramToMaskPipeline(window_size)(np_dist)
+
+    plt.imshow(binary_mask_tensor, cmap="gray")
     plt.savefig("tests/test_generate_img_crop.png")
     plt.close()
-    plt.imshow(mask[0][0])
+    
+    plt.imshow(mask.transpose([0,3,2,1])[0].astype(float))
     plt.savefig("tests/test_generate_mask.png")
     plt.close()
+    assert mask is not None, "Generated mask is None"
 
 
-# %%
+def test_encoder(model, distance_matrix_tensor):
+    # Test the encoder part of the model
+    z = model.encoder(distance_matrix_tensor)
+    assert z is not None, "Encoder output is None"
