13ccee201006b903413cf445cbaecad9db723803
Craig Russell <craig.russell.phd@gmail.com>
2023-11-28 13:29:34 +0000

    Updating testing for factory

--- bioimage_embed/models/tests/factory.py
+++ bioimage_embed/models/tests/factory.py
@@ -6,16 +6,25 @@ from bioimage_embed.models import MODELS
 
 # MODELS = ["resnet18_vae", "resnet50_vae", "resnet18_vqvae", "resnet50_vqvae"]
 
-input_dim = [(3,256,256),(3,224,224),(1,256,256),(1,224,224)]
-latent_dim = [50,16]
-pretrained = False
-progress = True
+# input_dim = [(3, 256, 256), (3, 224, 224),(1, 224, 224)]
+image_dim = [(256, 256), (224, 224)]
+channel_dim = [3,1]
+latent_dim = [64, 16]
+pretrained_options = [True, False]
+progress_options = [True, False]
+
 
 @pytest.mark.parametrize("model", MODELS)
-@pytest.mark.parametrize("latent_dim", latent_dim)
-@pytest.mark.parametrize("input_dim", input_dim)
-def test_create_model(model, input_dim, latent_dim, pretrained, progress):
-    model = create_model(model, input_dim, latent_dim, pretrained, progress)
-    data = torch.rand(1, *input_dim)
-    output = generated_model(data)
-    assert output.size() == data.size()
+@pytest.mark.parametrize("ld", latent_dim)
+@pytest.mark.parametrize("c", channel_dim)
+@pytest.mark.parametrize("idim", image_dim)
+@pytest.mark.parametrize("pretrained", pretrained_options)
+@pytest.mark.parametrize("progress", progress_options)
+def test_create_model(model, c,idim, ld, pretrained, progress):
+    generated_model = create_model(model, idim, ld, pretrained, progress)
+    data = torch.rand(1, c, *idim)
+    output = generated_model({"data": data})
+    assert output["z"].shape[1] == ld
+    assert output["recon_x"].shape == data.shape
+    if len(output["z"].flatten()) != ld:
+        pytest.skip("Not an exact latent dimension match")
