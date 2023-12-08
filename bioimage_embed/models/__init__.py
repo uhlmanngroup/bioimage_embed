271786957ce5a99f231e5084385bf85f14b8d60e
Craig Russell <craig.russell.phd@gmail.com>
2023-12-08 14:40:50 +0000

    cleaning up init

--- bioimage_embed/models/__init__.py
+++ bioimage_embed/models/__init__.py
@@ -4,21 +4,17 @@ import torch.nn.functional as F
 # Note - you must have torchvision installed for this example
 from torch.utils.data import DataLoader
 
-
-
 # from .ae import AutoEncoder
 
 # from .vae_bio import Mask_VAE, Image_VAE
 # from .utils import BaseVAE
 # from .legacy.vae import VAE
 # from .vq_vae import VQ_VAE
-from .legacy import BioimageEmbed
-
 
 from .bolts import ResNet18VAEEncoder, ResNet18VAEDecoder
 
 from . import bolts
 from . import pythae
-from .factory import ModelFactory, create_model, MODELS
+from .factory import ModelFactory, create_model, MODELS, CreateModel
 
 
