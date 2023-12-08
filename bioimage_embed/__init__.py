27dbbda78c3a0f33c9a1908baf62db288c2abdac
Craig Russell <craig.russell.phd@gmail.com>
2023-12-08 14:41:48 +0000

    Adding useful classes and modules to init

--- bioimage_embed/__init__.py
+++ bioimage_embed/__init__.py
@@ -1,3 +1,6 @@
 import torch
 torch.cuda.empty_cache()
-from . import models, lightning
+from . import models, lightning, cli, export, config
+from .models import create_model
+from .lightning import LitAutoEncoderTorch
+from .models import ModelFactory,CreateModel
