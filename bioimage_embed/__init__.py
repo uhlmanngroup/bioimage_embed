581f98fe6271e6ad1a56743075b7c70e194f8ff9
Craig Russell <craig.russell.phd@gmail.com>
2023-12-12 08:54:59 +0000

    commenting imports

--- bioimage_embed/__init__.py
+++ bioimage_embed/__init__.py
@@ -1,6 +1,6 @@
 import torch
 torch.cuda.empty_cache()
-from . import models, lightning, cli, export, config
+# from . import models, lightning, cli, export, config
 from .models import create_model
 from .lightning import LitAutoEncoderTorch
 from .models import ModelFactory,CreateModel
