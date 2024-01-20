a34ee720594c02af5aec304a7c521a3c5ee3a22e
Anna Foix <afoix@ebi.ac.uk>
2024-01-20 12:36:43 +0000

    local changes to run

--- scripts/shapes/shape_embed.py
+++ scripts/shapes/shape_embed.py
@@ -150,14 +150,14 @@ def shape_embed_process():
     args = SimpleNamespace(**params, **optimizer_params, **lr_scheduler_params)
 
     #dataset_path = "bbbc010/BBBC010_v1_foreground_eachworm"
-    dataset_path = "shape_embed_data/data/bbbc010/BBBC010_v1_foreground_eachworm/"
+    dataset_path = "bbbc010/BBBC010_v1_foreground_eachworm/"
     # dataset_path = "vampire/mefs/data/processed/Control"
     # dataset_path = "shape_embed_data/data/vampire/torchvision/Control/"
     # dataset_path = "vampire/torchvision/Control"
     # dataset = "bbbc010"
 
     # train_data_path = f"scripts/shapes/data/{dataset_path}"
-    train_data_path = f"data/{dataset_path}"
+    train_data_path = f"/nfs/research/uhlmann/afoix/{dataset_path}"
     metadata = lambda x: f"results/{dataset_path}_{args.model}/{x}"
 
     path = Path(metadata(""))
@@ -316,7 +316,6 @@ def shape_embed_process():
         callbacks=[checkpoint_callback],
         min_epochs=50,
         max_epochs=args.epochs,
-        callbacks=[EarlyStopping(monitor="loss/val", mode="min")],
         log_every_n_steps=1,
     )
     # %%
