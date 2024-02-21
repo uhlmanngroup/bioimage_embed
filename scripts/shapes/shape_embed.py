e85f4c11a3cd25f1364c730c2dc57212af6dad09
Craig Russell <craig.russell.phd@gmail.com>
2024-02-21 11:49:31 +0000

    Refactoring and adding back umap

--- scripts/shapes/shape_embed.py
+++ scripts/shapes/shape_embed.py
@@ -23,6 +23,8 @@ from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 import pytorch_lightning as pl
 import torch
 from types import SimpleNamespace
+from umap import UMAP
+import os
 
 # Deal with the filesystem
 import torch.multiprocessing
@@ -65,6 +67,41 @@ def hashing_fn(args):
     hash_object = hashlib.sha256(serialized_args)
     hashed_string = base64.urlsafe_b64encode(hash_object.digest()).decode()
     return hashed_string
+
+
+def umap_plot(df, metadata, width=3.45, height=3.45 / 1.618):
+    umap_reducer = UMAP(n_neighbors=15, min_dist=0.1, n_components=2, random_state=42)
+    mask = np.random.rand(len(df)) < 0.7
+
+    semi_labels = df["Class"].copy()
+    semi_labels[~mask] = -1  # Assuming -1 indicates unknown label for semi-supervision
+
+    umap_embedding = umap_reducer.fit_transform(df, y=semi_labels)
+
+    ax = sns.relplot(
+        data=pd.DataFrame(umap_embedding, columns=["umap0", "umap1"]),
+        x="umap0",
+        y="umap1",
+        hue="Class",
+        palette="deep",
+        alpha=0.5,
+        edgecolor=None,
+        s=5,
+        height=height,
+        aspect=0.5 * width / height,
+    )
+
+    sns.move_legend(
+        ax,
+        "upper center",
+    )
+    ax.set(xlabel=None, ylabel=None)
+    sns.despine(left=True, bottom=True)
+    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+    plt.tight_layout()
+    plt.savefig(metadata(f"umap_no_axes.pdf"))
+    # plt.show()
+    plt.close()
 
 def scoring_df(X, y):
     # Split the data into training and test sets
@@ -370,11 +407,6 @@ def shape_embed_process():
     idx_to_class = {v: k for k, v in dataset.dataset.class_to_idx.items()}
     y = np.array([int(data[-1]) for data in dataloader.predict_dataloader()])
 
-    y_partial = y.copy()
-    indices = np.random.choice(y.size, int(0.3 * y.size), replace=False)
-    y_partial[indices] = -1
-    y_blind = -1 * np.ones_like(y)
-    
     df = pd.DataFrame(latent_space.numpy())
     df["Class"] = y
     # Map numeric classes to their labels
@@ -384,31 +416,8 @@ def shape_embed_process():
     df = df.set_index("Class")
     df_shape_embed = df.copy()
 
-    ax = sns.relplot(
-        data=df,
-        x="umap0",
-        y="umap1",
-        hue="Class",
-        palette="deep",
-        alpha=0.5,
-        edgecolor=None,
-        s=5,
-        height=height,
-        aspect=0.5 * width / height,
-    )
-
-    sns.move_legend(
-        ax,
-        "upper center",
-    )
-    ax.set(xlabel=None, ylabel=None)
-    sns.despine(left=True, bottom=True)
-    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
-    plt.tight_layout()
-    plt.savefig(metadata(f"umap_no_axes.pdf"))
-    # plt.show()
-    plt.close()
-
+    # %% UMAP plot
+    umap_plot(df, metadata, width, height)
     # %%
 
     X = df_shape_embed.to_numpy()
