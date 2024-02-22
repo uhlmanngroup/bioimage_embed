d3525a8817d11db0960743d19eafb4a34a2ce590
Anna Foix <afoix@ebi.ac.uk>
2024-02-22 21:52:28 +0000

    Fix wandb logger

--- scripts/shapes/shape_embed.py
+++ scripts/shapes/shape_embed.py
@@ -367,10 +367,10 @@ def shape_embed_process(clargs):
         monitor="loss/val",
         mode="min",
     )
-    wandb.watch(lit_model, log="all")
+    wandblogger.watch(lit_model, log="all")
     
     trainer = pl.Trainer(
-        logger=[wandb, tb_logger],
+        logger=[wandblogger, tb_logger],
         gradient_clip_val=0.5,
         enable_checkpointing=True,
         devices=1,
