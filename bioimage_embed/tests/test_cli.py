566039291bce7dd998082827d3cad559048f1174
Craig Russell <ctr26@ebi.ac.uk>
2024-03-20 11:15:26 +0000

    Adding cli tests

--- bioimage_embed/tests/test_cli.py
+++ bioimage_embed/tests/test_cli.py
@@ -1,7 +1,7 @@
 import os
 import pytest
-from .. import hydra_cli
 from hydra import initialize, compose
+from .. import cli
 from pathlib import Path
 import os
 import pytest
@@ -41,12 +41,12 @@ def test_write_default_config_file(
     config_path, config_dir, config_file, config_directory_setup
 ):
     # config_path, config_file = config_directory_setup
-    hydra_cli.write_default_config_file(config_path)
+    cli.write_default_config_file(config_path)
     assert config_path.is_file(), "Default config file was not created"
 
 
 def test_get_default_config():
-    cfg = hydra_cli.get_default_config()
+    cfg = cli.get_default_config()
     assert cfg is not None, "Default config should not be None"
     # Further assertions can be added to check specific config properties
 
@@ -54,11 +54,11 @@ def test_get_default_config():
 def test_main_with_default_config(
     config_path, config_dir, config_file, config_directory_setup
 ):
-    hydra_cli.write_default_config_file(config_path)
+    cli.write_default_config_file(config_path)
     assert config_path.is_file()
     # assert os.path.isfile(config_path), "Default config file was not created"
 
     # Now, test if main function works correctly with this default configuration
-    hydra_cli.main(config_path=config_dir, job_name="test_app")
+    cli.main(config_path=config_dir, job_name="test_app")
     # Add your logic here to validate the proper initialization of the configuration
     # This might involve checking if certain expected values are set in the configuration
