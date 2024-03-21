9a10beb7411d904c32296455dd727114c65b9d6b
Craig Russell <ctr26@ebi.ac.uk>
2024-03-21 13:26:17 +0000

    Overhauled cli fully (needs config changes to work)

--- bioimage_embed/cli.py
+++ bioimage_embed/cli.py
@@ -1,84 +1,20 @@
-# from .hydra_cli import train, infer
-# from typer import Typer
-
-# app = Typer()
-# app.command()(train)
-# app.command()(infer)
-
-# def main():
-#     app()
-    
-# if __name__ == "__main__":
-#     main()
-
-from hydra.core.config_store import ConfigStore
+from typer import Typer
+from bioimage_embed.bioimage_embed import BioImageEmbed
 from dataclasses import dataclass
-from hydra import compose, initialize
-from omegaconf import OmegaConf
-from types import SimpleNamespace
-import hydra
-from hydra.core.config_store import ConfigStore
-from omegaconf import OmegaConf
-import albumentations
-from dataclasses import dataclass, field
-from bioimage_embed.augmentations import (
-    DEFAULT_AUGMENTATION_LIST,
-    DEFAULT_AUGMENTATION,
-    DEFAULT_AUGMENTATION_DICT,
-)
-import albumentations as A
-import os
-from typing import Optional
-from omegaconf import DictConfig, OmegaConf
-from pathlib import Path
 import torch
-import bioimage_embed
-from pydantic import BaseModel, conint, validator
-from pydantic.dataclasses import dataclass
-from bioimage_embed.lightning import DataModule
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from .lightning.torch import LitAutoEncoderTorch
-from pytorch_lightning.callbacks import EarlyStopping, Callback
-from typing import List, Optional
-from . import config
+
+from omegaconf import OmegaConf
+from hydra import compose, initialize
+from hydra.core.config_store import ConfigStore
+
+from .config import Config
+import hydra
+
+app = Typer()
 
 
 cs = ConfigStore.instance()
-cs.store(name="config", node=config.Config)
-
-from hydra.core.config_store import ConfigStore
-from dataclasses import dataclass
-from hydra import compose, initialize
-from omegaconf import OmegaConf
-from types import SimpleNamespace
-import hydra
-from hydra.core.config_store import ConfigStore
-from omegaconf import OmegaConf
-import albumentations
-from dataclasses import dataclass, field
-from bioimage_embed.augmentations import (
-    DEFAULT_AUGMENTATION_LIST,
-    DEFAULT_AUGMENTATION,
-    DEFAULT_AUGMENTATION_DICT,
-)
-import albumentations as A
-import os
-from typing import Optional
-from omegaconf import DictConfig, OmegaConf
-from pathlib import Path
-import torch
-import bioimage_embed
-from pydantic import BaseModel, conint, validator
-from pydantic.dataclasses import dataclass
-from bioimage_embed.lightning import DataModule
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from .lightning.torch import LitAutoEncoderTorch
-from pytorch_lightning.callbacks import EarlyStopping, Callback
-from typing import List, Optional
-from .config import Config, Recipe, Transform
-from torch.autograd import Variable
-from hydra.utils import instantiate
-
+cs.store(name="config", node=Config)
 
 
 def write_default_config_file(config_path):
@@ -93,17 +29,26 @@ def write_default_config_file(config_path):
 #     print(cfg)
 
 
-def main(config_dir="conf", config_file="config.yaml", job_name="test_app"):
-    hydra.initialize(version_base=None, config_path=config_dir, job_name=job_name)
-    cfg = hydra.compose(config_name=config_file)
-    return cfg
+@hydra.main(config_path=".", config_name="config")
+def train(cfg: Config):
+    # print(OmegaConf.to_yaml(cfg))
+    bie = BioImageEmbed(cfg)
+    bie.train()
+
+
+def init_hydra(config_dir="conf", config_file="config.yaml", job_name="bie"):
+    hydra.initialize(
+        version_base=None,
+        config_path=config_dir,
+        job_name=job_name,
+    )
+    return hydra.compose(config_name=config_file)
 
 
 def get_default_config(config_name="config"):
     with initialize(config_path=None, version_base=None):
         cfg = compose(config_name=config_name)
     return cfg
-
 
 
 def filter_dataset(dataset: torch.Tensor):
@@ -118,3 +63,21 @@ def filter_dataset(dataset: torch.Tensor):
             # A better way to do with would be with batch collation
             print(f"Error occurred for image {idx}: {e}")
         return torch.utils.data.Subset(dataset, valid_indices)
+
+
+if __name__ == "__main__":
+    train()
+
+# def infer():
+#     main(job_name="test_app")
+
+
+# def main():
+#     app()
+
+# if __name__ == "__main__":
+#     main()
+
+
+# app.command()(train)
+# app.command()(infer)
