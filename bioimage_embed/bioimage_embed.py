f83af1c51686f13ca229cec321990a5f10b844a8
Craig Russell <ctr26@ebi.ac.uk>
2024-04-03 12:23:21 +0100

    Updating the bie.bie to work more cleanly with smarter config

--- bioimage_embed/bioimage_embed.py
+++ bioimage_embed/bioimage_embed.py
@@ -17,10 +17,12 @@ from bioimage_embed.lightning import DataModule
 from .lightning.torch import LitAutoEncoderTorch
 from hydra.utils import instantiate
 from torch.autograd import Variable
+from pytorch_lightning import seed_everything
 
 # Environment Setup
 # torch.multiprocessing.set_sharing_strategy("file_system")
 logging.basicConfig(level=logging.INFO)
+from . import utils
 
 
 class BioImageEmbed:
@@ -28,80 +30,94 @@ class BioImageEmbed:
 
     def __init__(self, cfg: Config):
         self.cfg = cfg
+        self.icfg = instantiate(cfg)
 
-        # self.logger = initialize(cfg.logger)
-        # a_wrapper = lambda x: self.albumentation(image=x)["image"]
-        cfg.dataset.root = cfg.recipe.data
-        cfg.dataset.transform = cfg.transform
-
-        cfg.lit_model.model = cfg.model
-        cfg.lit_model.args = cfg.recipe
-        # self.albumentation = instantiate(cfg.transform)
-        # TODO this maybe a hack to get albumentations to work
-        # I think this can be done in the original dataclass
-        # self.transform_dict = instantiate(cfg.transform.transform_dict, _convert_="object")
-
-        self.recipe = instantiate(cfg.recipe)
-        self.dataloader = instantiate(cfg.dataloader, dataset=cfg.dataset)
-        # self.callbacks = initialize(cfg.callbacks)
-        self.callbacks = None
-        self.trainer = instantiate(cfg.trainer)
-        self.paths = instantiate(cfg.paths)
-        self.lit_model = instantiate(cfg.lit_model)
         self.setup()
 
+    def checkpoint_hash(self):
+        recipe = self.icfg.recipe
+        self.checkpoint_dir = utils.hashing_fn(recipe)
+
     def setup(self):
+        np.random.seed(self.icfg.recipe.seed)
+        seed_everything(self.icfg.recipe.seed)
         self.make_dirs()
-        self.dataloader.setup()
-        self.lit_model.model.eval()
+        self.icfg.lit_model.model.eval()
 
     def model_check(self):
-        data = self.dataloader.dataset[0]
-        dataloader_0 = next(iter(self.dataloader.train_dataloader()))
-        output = self.lit_model(dataloader_0)
+        dataloader = self.icfg.dataloader
+        dataloader_0 = next(iter(dataloader.train_dataloader()))
+        output = self.icfg.lit_model(dataloader_0)
         logging.info("Model Check Passed")
 
     def trainer_check(self):
         trainer = instantiate(self.cfg.trainer, fast_dev_run=True)
-        trainer.fit(self.lit_model, self.dataloader)
+        trainer.fit(self.icfg.lit_model, self.icfg.dataloader)
         logging.info("Trainer Check Passed")
 
     # TODO fix resume
     def make_dirs(self):
-        for path in self.paths.values():
+        for path in (self.icfg.paths).values():
             os.makedirs(path, exist_ok=True)
 
+    def find_checkpoint(self):
+        for callback in self.icfg.trainer.callbacks:
+            if isinstance(callback, ModelCheckpoint):
+                return callback.last_model_path
+
     def train(self, resume: bool = True):
+        # Find this dynamically
+        # chkpt_callbacks = self.icfg.trainer.callbacks[-1]
+        chkpt_callbacks = self.find_checkpoint()
+
+        last_checkpoint = chkpt_callbacks.last_model_path
+        best_checkpoint_path = chkpt_callbacks.best_model_path
+        # TODO better than try except
         try:
-            self.trainer.fit(
-                self.lit_model,
-                datamodule=self.dataloader,
-                ckpt_path=f"{self.cfg.paths.model}/last.ckpt",
+            self.icfg.trainer.fit(
+                self.icfg.lit_model,
+                datamodule=self.icfg.dataloader,
+                ckpt_path=last_checkpoint,
             )
         except:
-            self.trainer.fit(self.lit_model, datamodule=self.dataloader)
+            self.icfg.trainer.fit(self.icfg.lit_model, datamodule=self.icfg.dataloader)
 
     def validate(self):
-        validation = self.trainer.validate(self.lit_model, datamodule=self.dataloader)
-        # testing = trainer.test(lit_model, datamodule=dataloader)
+        validation = self.icfg.trainer.validate(
+            self.lit_model, datamodule=self.dataloader
+        )
+
+    def test(self):
+        testing = self.icfg.trainer.test(
+            self.icfg.lit_model, datamodule=self.icfg.dataloader
+        )
 
     def __call__(self, x):
-        return self.lit_model(x)
+        return self.icfg.lit_model(x)
 
     def infer(self):
         dataloader = DataModule(
-            self.dataset,
+            self.icfg.dataset,
             batch_size=1,
             shuffle=False,
-            num_workers=self.cfg.receipe.num_workers,
+            num_workers=self.icfg.receipe.num_workers,
             # TODO: Add transform here if batch_size > 1 assuming averaging?
             # Transform is commented here to avoid augmentations in real data
             # HOWEVER, applying a the transform multiple times and averaging the results might produce better latent embeddings
             # transform=transform,
         )
-        dataloader.setup()
-        predictions = self.trainer.predict(self.lit_model, datamodule=dataloader)
+        # dataloader.setup()
+        predictions = self.icfg.trainer.predict(
+            self.icfg.lit_model, datamodule=dataloader
+        )
         return predictions
 
     def export(self):
         example_input = Variable(torch.rand(1, *self.cfg.recipe.input_dim))
+        self.icfg.lit_model.to_onnx(
+            f"{self.icfg.uuid}.onnx",
+            example_input,
+            export_params=True,
+            # opset_version=11,
+            verbose=True,
+        )
