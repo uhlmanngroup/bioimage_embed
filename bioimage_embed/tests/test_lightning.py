d114cbbb2546079d0da441767708fbdf82882e93
Craig Russell <ctr26@ebi.ac.uk>
2024-05-31 17:30:48 +0100

    [bugs] fixing all lightning tests

--- bioimage_embed/tests/test_lightning.py
+++ bioimage_embed/tests/test_lightning.py
@@ -1,9 +1,9 @@
-from bioimage_embed.models import create_model
 import pytest
 import torch
 import pytorch_lightning as pl
+from bioimage_embed.lightning import AutoEncoderSupervised,AutoEncoderUnsupervised
 from bioimage_embed.models import create_model, MODELS
-from bioimage_embed.models import MODELS
+from torch.utils.data import TensorDataset
 from bioimage_embed.lightning import DataModule
 from bioimage_embed.lightning.torch import _3c_model_classes
 
@@ -66,32 +66,39 @@ def input_dim(image_dim, channel_dim):
 
 @pytest.fixture()
 def data(input_dim):
-    return torch.rand(1, *input_dim)
+    return torch.rand(*input_dim)
 
+@pytest.fixture()
+def dataset(data):
+    return data.unsqueeze(0)
+
+@pytest.fixture()
+def labelled_dataset(data):
+    return TensorDataset(data, torch.zeros(1))
 
 
 @pytest.fixture()
-def lit_model(model, model_class):
-    return model_class(model)
-    return LitAutoEncoderTorch(model)
-
-def test_export_onxx(data, lit_model):
-    return lit_model.to_onnx("model.onnx", data)
+def unlabelled_dataset(data):
+    return data
 
 @pytest.fixture()
-def model_torchscript(lit_model):
-    return lit_model.to_torchscript()
+def supervised_lit_model(model):
+    return AutoEncoderSupervised(model)
 
+@pytest.fixture()
+def unsupervised_lit_model(model):
+    return AutoEncoderUnsupervised(model)
 
-@pytest.mark.skip(reason="Upstream bug with pythae")
-def test_export_jit(data, model_torchscript):
-    return model_torchscript.save("model.pt")
+def test_export_onxx_unsupervised(data, unsupervised_lit_model):
+    return unsupervised_lit_model.to_onnx("model.onnx", data)
 
+@pytest.fixture()
+def labelled_data(data):
+    return data, torch.tensor([0])
 
-@pytest.mark.skip(reason="Upstream bug with pythae")
-def test_jit_save(model_torchscript):
-    return torch.jit.save(model_torchscript, "model.pt", method="script")
-
+@pytest.mark.skip(reason="Dictionaries not allowed")
+def test_export_onxx_supervised(data, supervised_lit_model):
+    return supervised_lit_model.to_onnx("model.onnx", data)
 
 @pytest.fixture()
 def data(input_dim):
@@ -99,16 +106,31 @@ def data(input_dim):
 
 
 @pytest.fixture()
-def dataset(data):
-    return data
+def labelled_dataloader(labelled_dataset, batch_size):
+    return DataModule(
+        labelled_dataset,
+        batch_size=batch_size,
+        # shuffle=True,
+        num_workers=1,
+        pin_memory=False,
+    )
 
+@pytest.fixture()
+def unlabelled_dataloader(unlabelled_dataset, batch_size):
+    return DataModule(
+        unlabelled_dataset,
+        batch_size=batch_size,
+        # shuffle=True,
+        num_workers=1,
+        pin_memory=False,
+    )
 
 @pytest.fixture()
 def dataloader(dataset, batch_size):
     return DataModule(
         dataset,
         batch_size=batch_size,
-        shuffle=True,
+        # shuffle=True,
         num_workers=1,
         pin_memory=False,
     )
@@ -121,19 +143,39 @@ def trainer():
         max_epochs=1,
     )
 
-@pytest.mark.skip(reason="Expensive to run")
-def test_trainer_fit(trainer, lit_model, dataloader):
-    return trainer.fit(lit_model, dataloader)
+def test_trainer_test_supervised(trainer,supervised_lit_model,labelled_dataloader):
+    return trainer.test(supervised_lit_model, labelled_dataloader)
 
-@pytest.fixture()
-def tensor(dataset):
-    return dataset.unsqueeze(0)
-    
-def test_dataset(lit_model, dataset):
-    return lit_model(dataset)
+def test_trainer_test_unsupervised(trainer,unsupervised_lit_model,unlabelled_dataloader):
+    return trainer.test(unsupervised_lit_model, unlabelled_dataloader)
 
-def test_dataset_trainer(trainer, lit_model, dataset):
-    return trainer.test(lit_model, dataset)
+@pytest.mark.skip(reason="Expensive")
+def test_trainer_fit_supervised(trainer,supervised_lit_model,labelled_dataloader):
+    return trainer.fit(supervised_lit_model, labelled_dataloader)
 
-def test_dataloader_trainer(trainer, lit_model, dataloader):
-    return trainer.test(lit_model, dataloader)
+@pytest.mark.skip(reason="Expensive")
+def test_trainer_fit_unsupervised(trainer,unsupervised_lit_model,unlabelled_dataloader):
+    return trainer.fit(unsupervised_lit_model, unlabelled_dataloader)
+
+def test_dataset_trainer(trainer, supervised_lit_model, labelled_dataset):
+    return trainer.test(supervised_lit_model, labelled_dataset)
+
+def test_dataset_trainer(trainer, unsupervised_lit_model, unlabelled_dataset):
+    return trainer.test(unsupervised_lit_model, unlabelled_dataset)
+
+@pytest.mark.skip(reason="Dictionaries not allowed")
+def test_export_onnx_supervised(data, supervised_lit_model):
+    return supervised_lit_model.to_onnx("model.onnx", data)
+
+def test_export_onnx_unsupervised(data, unsupervised_lit_model):
+    return unsupervised_lit_model.to_onnx("model.onnx", data)
+
+@pytest.mark.skip(reason="Upstream bug with pythae")
+def test_export_jit(data, model_torchscript):
+    return model_torchscript.save("model.pt")
+
+
+@pytest.mark.skip(reason="Upstream bug with pythae")
+def test_jit_save(model_torchscript):
+    return torch.jit.save(model_torchscript, "model.pt", method="script")
+
