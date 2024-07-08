783c4649a0b2703b37f647575a4a022efaa065c1
Craig Russell <ctr26@ebi.ac.uk>
2024-07-08 18:19:02 +0100

    [bug] imports for bie are needed

--- bioimage_embed/__init__.py
+++ bioimage_embed/__init__.py
@@ -5,13 +5,13 @@ torch.cuda.empty_cache()
 # from .lightning import AutoEncoderSupervised, AutoEncoderUnsupervised
 
 # TODO: Fix this import as it currently produces too many warnings
-# from .models import ModelFactory, create_model
-# from .bioimage_embed import BioImageEmbed
-# from .config import Config
-# from . import augmentations
+from .models import ModelFactory, create_model
+from .bioimage_embed import BioImageEmbed
+from .config import Config
+from . import augmentations
 
-# import logging
-# logging.captureWarnings(True)
+import logging
+logging.captureWarnings(True)
 
 __all__ = [
     "AutoEncoderSupervised",
