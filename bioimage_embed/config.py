06eb975118aa4b02b694f8b584f2c6f718180dcd
Craig Russell <ctr26@ebi.ac.uk>
2024-07-10 11:11:33 +0100

    [bug] Everything needs to be Any (for now)

--- bioimage_embed/config.py
+++ bioimage_embed/config.py
@@ -34,6 +34,7 @@ class Recipe:
     batch_size: int = 16
     data: str = "data"
     opt: str = "adamw"
+    latent_dim: int = 64
     batch_size: int = 16
     max_epochs: int = 125
     weight_decay: float = 0.001
@@ -136,7 +137,7 @@ class Model:
     _target_: Any = "bioimage_embed.models.create_model"
     model: str = II("recipe.model")
     input_dim: List[int] = Field(default_factory=lambda: [3, 224, 224])
-    latent_dim: int = 64
+    latent_dim: int = II("recipe.latent_dim")
     pretrained: bool = True
 
 
@@ -180,14 +181,15 @@ class LightningModelSupervised(LightningModel):
 class Callbacks:
     # _target_: str = "collections.OrderedDict"
     model_checkpoint: Any = Field(default_factory=ModelCheckpoint)
-    early_stopping: Any = Field(default_factory=EarlyStopping)
+    # early_stopping: Any = Field(default_factory=EarlyStopping)
 
 
 
 @dataclass(config=dict(extra="allow"))
 class Trainer:
+# class Trainer(pytorch_lightning.Trainer):
     _target_: Any = "pytorch_lightning.Trainer"
-    logger: Optional[List[Any]] = Field(default_factory=List)
+    logger: Any = None
     gradient_clip_val: float = 0.5
     enable_checkpointing: bool = True
     devices: Any = "auto"
@@ -195,13 +197,13 @@ class Trainer:
     accumulate_grad_batches: int = 16
     min_epochs: int = 1
     max_epochs: int = II("recipe.max_epochs")
+    num_nodes: int = 1,
     log_every_n_steps: int = 1
     # This is not a clean implementation but I am not sure how to do it better
     callbacks: Any = Field(
         default_factory=lambda: list(vars(Callbacks()).values()), frozen=True
     )
     # TODO idea here would be to use pydantic to validate omegaconf
-
 
 # TODO add argument caching for checkpointing
 
