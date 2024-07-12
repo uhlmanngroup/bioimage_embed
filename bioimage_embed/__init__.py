702b00ef2ce24a04ecb81123cf7ced7d052bfbf7
Craig Russell <ctr26@ebi.ac.uk>
2024-07-12 21:03:51 +0100

    [bug] fixing bioimage_embed init

--- bioimage_embed/__init__.py
+++ bioimage_embed/__init__.py
@@ -1,11 +1,23 @@
-import torch
-torch.cuda.empty_cache()
-# from . import models, lightning, cli, export, config
-from .lightning import AutoEncoderSupervised, AutoEncoderUnsupervised
-# TODO: Fix this import as it currently produces too many warnings
-# from .models import ModelFactory, create_model
 from .bioimage_embed import BioImageEmbed
+from .models import ModelFactory, create_model
 from .config import Config
 from . import augmentations
+from .lightning import AutoEncoderSupervised, AutoEncoderUnsupervised
+import torch
+
+torch.cuda.empty_cache()
+# from . import models, lightning, cli, export, config
+# TODO: Fix this import as it currently produces too many warnings
 # import logging
 # logging.captureWarnings(True)
+
+# Defining the public API
+__all__ = [
+    "AutoEncoderSupervised",
+    "AutoEncoderUnsupervised",
+    "BioImageEmbed",
+    "Config",
+    "augmentations",
+    "ModelFactory",
+    "create_model",
+]
