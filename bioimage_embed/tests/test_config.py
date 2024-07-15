24477456810afec92da46b80f3f61eb5f82f368f
Craig Russell <ctr26@ebi.ac.uk>
2024-07-15 22:04:26 +0100

    [bug] Fixed the need to have a fake data omega conf config

--- bioimage_embed/tests/test_config.py
+++ bioimage_embed/tests/test_config.py
@@ -2,6 +2,8 @@ from .. import config
 from .. import bioimage_embed
 import pytest
 from hydra.utils import instantiate
+from torchvision.datasets import FakeData
+from torchvision import transforms
 
 schema_map = config.__schemas__
 schemas = list(schema_map.values())
@@ -20,18 +22,27 @@ def test_instantiate(Schema):
     obj = instantiate(Schema())
     assert obj is not None, "obj should not be None"
 
+@pytest.fixture
+def input_dim(): 
+    return [3, 224, 224]
 
 @pytest.fixture
-def cfg():
-    # dataset = config.ImageFolderDataset(root="data")
-    input_dim = [3, 224, 224]
-    mock_dataset = config.ImageFolderDataset(
-        _target_="bioimage_embed.datasets.FakeImageFolder",
-        image_size=input_dim,
-        num_classes=1,
-    )
-    dataloader = config.DataLoader(dataset=mock_dataset)
-    model = config.Model(input_dim=input_dim)
+def dataset(input_dim):
+    transform = transforms.ToTensor()
+    return FakeData(size=64,
+                       image_size=input_dim,
+                       num_classes=1,
+                       transform=transform)
+@pytest.fixture
+def dataloader(dataset):
+    return config.DataLoader(dataset=dataset, num_workers=0)
+
+@pytest.fixture
+def model(input_dim):
+    return config.Model(input_dim=input_dim)
+
+@pytest.fixture
+def cfg(dataloader,model):
     return config.Config(dataloader=dataloader, model=model)
 
 
@@ -42,6 +53,8 @@ def test_config(cfg):
 def test_config_instantiate(cfg):
     assert instantiate(cfg) is not None, "Config should not be None"
 
+def test_resolve(bie):
+    assert bie.resolve()
 
 @pytest.fixture
 def bie(cfg):
@@ -52,14 +65,5 @@ def test_model_check(bie):
     bie.model_check()
 
 
-@pytest.mark.skip("Computationally heavy")
 def test_train_check(bie):
     bie.trainer_check()
-
-
-# def mock_dataset(root="data", *args, **kwargs):
-#     return config.ImageFolderDataset(
-#         _target_="torchvision.datasets.FakeData", *args, **kwargs
-#     )
-
-# def MockDataset(torchvision.datasets.FakeData)
