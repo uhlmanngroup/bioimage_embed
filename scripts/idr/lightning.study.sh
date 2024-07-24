40db254c89969e5b5ecb32a3e93bcfa81d5b9dfc
Craig Russell <ctr26@ebi.ac.uk>
2024-07-24 11:45:14 +0100

    [feat] example of multinode training using lightning

--- scripts/idr/lightning.study.sh
+++ scripts/idr/lightning.study.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --nodes=3
+#SBATCH --gres=gpu:4
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=0-02:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH --job-name=lightning
 #SBATCH --constraint=a100  # Ensure the job is scheduled on nodes with A100 GPUs
 #SBATCH --mem-per-cpu=2GB
@@ -10,32 +10,21 @@
 #SBATCH --output=lightning_%j.out
 set -x
 
-source ./env/bin/activate
+source activate $1
 
 # debugging flags (optional)
 export NCCL_DEBUG=INFO
 export PYTHONFAULTHANDLER=1
+# export NCCL_P2P_DISABLE=1
+# unset LOCAL_RANK
 
 # on your cluster you might need these:
 # set the network interface
-export NCCL_SOCKET_IFNAME=^docker0,lo
+# export NCCL_SOCKET_IFNAME=^docker0,lo
 
 # might need the latest CUDA
 # module load NCCL/2.4.7-1-cuda.10.0
 
 # run script from above
 echo "Starting Lightning training script"
-srun python3 -u scripts/idr/study.py
-
-
-# # shellcheck disable=SC2206
-# # SBATCH --job-name=lightning
-# # SBATCH --nodes=4             # This needs to match Trainer(num_nodes=...)
-# # SBATCH --ntasks-per-node=1   # This needs to match Trainer(devices=...)
-# # SBATCH --cpus-per-task=16
-# # SBATCH --mem-per-cpu=2GB
-# # SBATCH --tasks-per-node=1
-# # SBATCH --gpus-per-task=1
-# # SBATCH --constraint=a100  # Ensure the job is scheduled on nodes with A100 GPUs
-# # SBATCH --output=lightning_%j.out
-# # SBATCH --time=24:00:00
+srun python3 scripts/idr/study.py
