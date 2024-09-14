f7c9b7495256f38e9d4dc8aa0db2746146233cef
ctr26 <craig.russell.phd@gmail.com>
2024-09-14 15:22:22 +0100

    [ref] ruff format

--- bioimage_embed/__init__.py
+++ bioimage_embed/__init__.py
@@ -1,8 +1,12 @@
-import torch
-
-torch.cuda.empty_cache()
 # from . import models, lightning, cli, export, config
-from .lightning import AESupervised, AEUnsupervised, AE,  AutoEncoderSupervised, AutoEncoderUnsupervised, AutoEncoder
+from .lightning import (
+    AESupervised,
+    AEUnsupervised,
+    AE,
+    AutoEncoderSupervised,
+    AutoEncoderUnsupervised,
+    AutoEncoder,
+)
 
 # TODO: Fix this import as it currently produces too many warnings
 from .models import ModelFactory, create_model
@@ -13,14 +17,19 @@ from . import augmentations
 import logging
 logging.captureWarnings(True)
 
+import torch
+
+torch.cuda.empty_cache()
 __all__ = [
     "AESupervised",
     "AutoEncoderUnsupervised",
     "AEUnsupervised",
     "AutoEncoderSupervised",
-    "AutoEncoder"
-    "AE"
+    "AutoEncoder",
+    "AE",
     "BioImageEmbed",
     "Config",
     "augmentations",
+    "ModelFactory",
+    "create_model",
 ]
--- bioimage_embed/models/__init__.py
+++ bioimage_embed/models/__init__.py
@@ -1,18 +1,19 @@
-import torch
-import torch.nn.functional as F
+# Description: This file is the main entry point for the models module. It imports all the necessary modules and classes for the models module to function properly.
 
 # Note - you must have torchvision installed for this example
-from torch.utils.data import DataLoader
-
-# from .ae import AutoEncoder
-
-# from .vae_bio import Mask_VAE, Image_VAE
-# from .utils import BaseVAE
-# from .legacy.vae import VAE
-# from .vq_vae import VQ_VAE
 
 from .bolts import ResNet18VAEEncoder, ResNet18VAEDecoder
 
 from . import bolts
 from . import pythae
 from .factory import ModelFactory, create_model, __all_models__
+
+__all__ = [
+    "ModelFactory",
+    "create_model",
+    "__all_models__",
+    "ResNet18VAEEncoder",
+    "ResNet18VAEDecoder",
+    "bolts",
+    "pythae",
+]
